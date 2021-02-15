8d85a3d65d4bba2d9f54302944c76f4f1832540b
Vince Jankovics <vincejankovics@gmail.com>
2021-02-15 17:33:22 +0000

    Code dup removed

--- all_exp.py
+++ all_exp.py
@@ -1,7 +1,8 @@
-from wrappers.train_eval import train_and_eval
-import sys
-import yaml
+from argparse import Namespace
 
+from spg_exp import main
+
+args = Namespace()
 if __name__ == "__main__":
     for index_exp in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
         for environment in [
@@ -10,34 +11,14 @@ if __name__ == "__main__":
             "spg_dispenser_6rooms",
             "spg_coinmaster_singleroom",
         ]:
-            for sensor in ["rgb", "rgb_depth rgb_touch", "rgb_depth_touch"]:
+            for sensors in ["rgb", "rgb_depth rgb_touch", "rgb_depth_touch"]:
                 for entropy in [0.05, 0.01, 0.005, 0.001]:
                     for multistep in [0, 2, 3, 4]:
-                        fname = (
-                            environment
-                            + "_"
-                            + sensors
-                            + "_"
-                            + str(entropy)
-                            + "_"
-                            + str(multistep)
-                            + "_"
-                            + str(index_exp)
-                        )
-
-                        agent_type = "base"
-                        results = train_and_eval(
-                            agent_type,
-                            sensors,
-                            total_timesteps_training=int(1e6),
-                            n_multisteps=multistep,
-                            playground_name=environment,
-                            freq_eval=int(1e5),
-                            episodes_eval=5,
+                        args = Namespace(
+                            index_exp=index_exp,
+                            environment=environment,
+                            sensors=sensors,
                             entropy=entropy,
+                            multistep=multistep,
                         )
-
-                        log_fname = "logs/" + fname + ".dat"
-
-                        with open(log_fname, "w") as f:
-                            yaml.dump(results, f)
+                        main(args)
--- spg_exp.py
+++ spg_exp.py
@@ -1,44 +1,52 @@
-from spg_experiments.wrappers.train_eval import train_and_eval
-import sys
+import argparse
+
 import yaml
 
-if __name__ == "__main__":
+from spg_experiments.wrappers.train_eval import train_and_eval
 
+
+def main(args):
     # eval params
-    environment = sys.argv[1]
-    sensors = sys.argv[2]
-    entropy = float(sys.argv[3])
-    multistep = int(sys.argv[4])
-    if multistep == 0:
-        multistep = None
-    index_exp = int(sys.argv[5])
 
     fname = (
-        environment
+        args.environment
         + "_"
-        + sensors
+        + args.sensors
         + "_"
-        + str(entropy)
+        + str(args.entropy)
         + "_"
-        + str(multistep)
+        + str(args.multistep)
         + "_"
-        + str(index_exp)
+        + str(args.index_exp)
     )
 
     agent_type = "base"
 
     results = train_and_eval(
         agent_type,
-        sensors,
+        args.sensors,
         total_timesteps_training=int(1e6),
-        n_multisteps=multistep,
-        playground_name=environment,
+        n_multisteps=args.multistep,
+        playground_name=args.environment,
         freq_eval=int(1e5),
         episodes_eval=5,
-        entropy=entropy,
+        entropy=args.entropy,
     )
 
     log_fname = "logs/" + fname + ".dat"
 
     with open(log_fname, "w") as f:
         yaml.dump(results, f)
+
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument("--environment")
+    PARSER.add_argument("--sensors")
+    PARSER.add_argument("--entropy", type=float)
+    PARSER.add_argument("--multistep", type=int)
+    PARSER.add_argument("--index-exp", type=int)
+
+    ARGS = PARSER.parse_args()
+
+    main(ARGS)
