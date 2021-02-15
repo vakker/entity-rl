4bf5f3bc6935dfc8be45083266aa1d90070ba579
Vince Jankovics <vincejankovics@gmail.com>
2021-02-15 17:19:37 +0000

    Format, fix import

--- spg_exp.py
+++ spg_exp.py
@@ -1,26 +1,44 @@
-from wrappers.train_eval import train_and_eval
+from spg_experiments.wrappers.train_eval import train_and_eval
 import sys
 import yaml
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # eval params
     environment = sys.argv[1]
     sensors = sys.argv[2]
     entropy = float(sys.argv[3])
     multistep = int(sys.argv[4])
-    if multistep == 0: multistep = None
+    if multistep == 0:
+        multistep = None
     index_exp = int(sys.argv[5])
 
-    fname = environment + '_' + sensors + '_' + str(entropy) + '_' + str(multistep) + '_' + str(index_exp)
+    fname = (
+        environment
+        + "_"
+        + sensors
+        + "_"
+        + str(entropy)
+        + "_"
+        + str(multistep)
+        + "_"
+        + str(index_exp)
+    )
 
-    agent_type = 'base'
+    agent_type = "base"
 
-    results = train_and_eval(agent_type, sensors, total_timesteps_training=int(1e6),
-                    n_multisteps = multistep, playground_name = environment, freq_eval=int(1e5), episodes_eval=5, entropy = entropy)
+    results = train_and_eval(
+        agent_type,
+        sensors,
+        total_timesteps_training=int(1e6),
+        n_multisteps=multistep,
+        playground_name=environment,
+        freq_eval=int(1e5),
+        episodes_eval=5,
+        entropy=entropy,
+    )
 
+    log_fname = "logs/" + fname + ".dat"
 
-    log_fname = 'logs/' + fname + '.dat'
-
-    with open(log_fname, 'w') as f:
+    with open(log_fname, "w") as f:
         yaml.dump(results, f)
