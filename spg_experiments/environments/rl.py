f3e2465d5224fcf4b5363f64fc76a4bf6bac50a3
Vince Jankovics <vincejankovics@gmail.com>
2021-02-18 18:57:11 +0000

    Update experiments to match refactored SPG

--- spg_experiments/environments/rl.py
+++ spg_experiments/environments/rl.py
@@ -1,50 +1,52 @@
-from simple_playgrounds.playgrounds.empty import SingleRoom, ConnectedRooms2D, LinearRooms
-from simple_playgrounds.entities.scene_elements import *
-from simple_playgrounds.entities.field import Field
-
-from simple_playgrounds.utils import PositionAreaSampler, Trajectory
-from simple_playgrounds.playgrounds.playground import PlaygroundRegister
-
 import random
+
+from simple_playgrounds.playgrounds import PlaygroundRegister, scene_elements
+from simple_playgrounds.playgrounds.empty import (ConnectedRooms2D, LinearRooms,
+                                                  SingleRoom)
+from simple_playgrounds.utils import PositionAreaSampler, Trajectory
+
 
 @PlaygroundRegister.register('spg_endgoal_cue')
 class EndgoalRoomCue(SingleRoom):
-
     def __init__(self):
 
         # wall_texture = UniqueRandomTilesTexture(n_colors=4, delta_uniform=15, color_min=(50, 50, 50),
         #                                         color_max=(150, 150, 150), radius=100)
 
-        super().__init__(size = (200, 200))#, wall_texture = wall_texture)
+        super().__init__(size=(200, 200))  #, wall_texture = wall_texture)
 
         # Starting area of the agent
-        area_center, _ = self.area_rooms[(0,0)]
-        area_start = PositionAreaSampler(center=area_center, area_shape='rectangle', width_length=(100,100))
+        area_center, _ = self.area_rooms[(0, 0)]
+        area_start = PositionAreaSampler(center=area_center,
+                                         area_shape='rectangle',
+                                         width_length=(100, 100))
         self.agent_starting_area = area_start
 
         # Obstacles
-        obstacle_1 = Basic((60, 30, 0.34), default_config_key='pentagon')
+        obstacle_1 = scene_elements.Basic((60, 30, 0.34), default_config_key='pentagon')
         self.add_scene_element(obstacle_1)
 
-        obstacle_2 = Basic((130, 150, 1.7), default_config_key='rectangle')
+        obstacle_2 = scene_elements.Basic((130, 150, 1.7), default_config_key='rectangle')
         self.add_scene_element(obstacle_2)
 
-        obstacle_3 = Basic((40, 140, 0.4), default_config_key='square')
+        obstacle_3 = scene_elements.Basic((40, 140, 0.4), default_config_key='square')
         self.add_scene_element(obstacle_3)
 
-        obstacle_4 = Basic((160, 60, 0), physical_shape = 'triangle', radius = 14, texture = (150, 200, 200))
+        obstacle_4 = scene_elements.Basic((160, 60, 0),
+                                          physical_shape='triangle',
+                                          radius=14,
+                                          texture=(150, 200, 200))
         self.add_scene_element(obstacle_4)
 
         self.goal = None
         self.cue = None
 
-        self.goal_locations = ( (20, 20, 0), (180, 20, 0), (180, 180, 0), (20, 180, 0) )
-        self.cue_colors = ( (50, 50, 50), (50, 250, 50), (250, 50, 50), (50, 50, 250))
+        self.goal_locations = ((20, 20, 0), (180, 20, 0), (180, 180, 0), (20, 180, 0))
+        self.cue_colors = ((50, 50, 50), (50, 250, 50), (250, 50, 50), (50, 50, 250))
 
         self.set_goal()
         self.time_limit = 2000
         self.time_limit_reached_reward = -20
-
 
     def set_goal(self):
 
@@ -56,16 +58,22 @@ class EndgoalRoomCue(SingleRoom):
             self.remove_scene_element(self.goal)
             self.remove_scene_element(self.cue)
 
-        self.cue = Basic((100, 100, 0), physical_shape = 'circle', radius = 10, texture = col, is_temporary_entity = True)
+        self.cue = scene_elements.Basic((100, 100, 0),
+                                        physical_shape='circle',
+                                        radius=10,
+                                        texture=col,
+                                        is_temporary_entity=True)
         self.add_scene_element(self.cue)
 
-        self.goal = GoalZone(loc, reward = 10, is_temporary_entity = True)
+        self.goal = scene_elements.GoalZone(loc, reward=10, is_temporary_entity=True)
         self.add_scene_element(self.goal)
 
         for i in range(4):
             if i != index_goal:
                 loc = self.goal_locations[i]
-                other_goal = DeathZone(loc, reward=-5, is_temporary_entity=True)
+                other_goal = scene_elements.DeathZone(loc,
+                                                      reward=-5,
+                                                      is_temporary_entity=True)
                 self.add_scene_element(other_goal)
 
     def reset(self):
@@ -76,28 +84,28 @@ class EndgoalRoomCue(SingleRoom):
 
 @PlaygroundRegister.register('spg_endgoal_9rooms')
 class Endgoal9Rooms(ConnectedRooms2D):
-
     def __init__(self):
 
-        super().__init__(size = (600, 600), n_rooms=(3,3), wall_type='colorful')
+        super().__init__(size=(600, 600), n_rooms=(3, 3), wall_type='colorful')
 
         # Starting area of the agent
-        area_start = PositionAreaSampler(center=(300, 300), area_shape='rectangle', width_length=(600, 600))
+        area_start = PositionAreaSampler(center=(300, 300),
+                                         area_shape='rectangle',
+                                         width_length=(600, 600))
         self.agent_starting_area = area_start
 
         # invisible endzone at one corner of the game
-        invisible_endzone = GoalZone((20, 20, 0), reward = 10)
+        invisible_endzone = scene_elements.GoalZone((20, 20, 0), reward=10)
         self.add_scene_element(invisible_endzone)
 
         self.time_limit = 5000
 
+
 @PlaygroundRegister.register('spg_dispenser_6rooms')
 class DispenserEnv(ConnectedRooms2D):
-
     def __init__(self):
 
-        super().__init__(size = (300, 200), n_rooms=(3, 2))#, wall_type='colorful')
-
+        super().__init__(size=(300, 200), n_rooms=(3, 2))  #, wall_type='colorful')
 
         self.assign_areas()
         self.place_scene_elements()
@@ -110,7 +118,8 @@ class DispenserEnv(ConnectedRooms2D):
 
         # Starting area of the agent
         area_start_center, area_start_shape = self.area_rooms[list_room_coordinates.pop()]
-        area_start = PositionAreaSampler(center=area_start_center, area_shape='rectangle',
+        area_start = PositionAreaSampler(center=area_start_center,
+                                         area_shape='rectangle',
                                          width_length=area_start_shape)
         self.agent_starting_area = area_start
 
@@ -118,16 +127,22 @@ class DispenserEnv(ConnectedRooms2D):
         dispenser_center, dispenser_shape = self.area_rooms[list_room_coordinates.pop()]
         prod_center, prod_shape = self.area_rooms[list_room_coordinates.pop()]
 
-
-        self.area_prod = PositionAreaSampler(center=prod_center, area_shape='rectangle', width_length=prod_shape)
-        self.area_disp = PositionAreaSampler(center=dispenser_center, area_shape='rectangle', width_length=dispenser_shape)
+        self.area_prod = PositionAreaSampler(center=prod_center,
+                                             area_shape='rectangle',
+                                             width_length=prod_shape)
+        self.area_disp = PositionAreaSampler(center=dispenser_center,
+                                             area_shape='rectangle',
+                                             width_length=dispenser_shape)
 
     def place_scene_elements(self):
 
-
-        dispenser = Dispenser(self.area_disp, production_area = self.area_prod, entity_produced=Candy, radius = 10, is_temporary_entity = True, allow_overlapping = False )
+        dispenser = scene_elements.Dispenser(self.area_disp,
+                                             production_area=self.area_prod,
+                                             entity_produced=scene_elements.Candy,
+                                             radius=10,
+                                             is_temporary_entity=True,
+                                             allow_overlapping=False)
         self.add_scene_element(dispenser)
-
 
     def reset(self):
         self.assign_areas()
@@ -135,10 +150,12 @@ class DispenserEnv(ConnectedRooms2D):
             agent.initial_position = self.agent_starting_area
         super().reset()
         self.place_scene_elements()
+
+
 #
 #
 # @PlaygroundRegister.register('spg_coinmaster')
-# class CoinMaster(ConnectedRooms2D):
+# class scene_elements.CoinMaster(ConnectedRooms2D):
 #
 #     def __init__(self):
 #
@@ -146,7 +163,7 @@ class DispenserEnv(ConnectedRooms2D):
 #
 #         self.assign_areas()
 #
-#         self.field = Field(Coin, self.area_prod, limit=5, total_limit=1000,
+#         self.field = scene_elements.Field(scene_elements.Coin, self.area_prod, limit=5, total_limit=1000,
 #                            entity_produced_params={'graspable': True, 'reward': 1,
 #                                                    'allow_overlapping': False})
 #         self.add_scene_element(self.field)
@@ -196,30 +213,38 @@ class DispenserEnv(ConnectedRooms2D):
 
 @PlaygroundRegister.register('spg_coinmaster_singleroom')
 class CoinMaster(SingleRoom):
-
     def __init__(self):
 
-        super().__init__(size = (200, 200), wall_type='dark')
+        super().__init__(size=(200, 200), wall_type='dark')
 
         self.assign_areas()
 
-        self.field = Field(Coin, self.area_prod, limit=5, total_limit=1000,
-                           entity_produced_params={'graspable': True, 'reward': 1})
+        self.field = scene_elements.Field(scene_elements.Coin,
+                                          self.area_prod,
+                                          limit=5,
+                                          total_limit=1000,
+                                          entity_produced_params={
+                                              'graspable': True,
+                                              'reward': 1
+                                          })
         self.add_scene_element(self.field)
 
-        self.vending_machine = VendingMachine(self.area_vm, allow_overlapping=False, reward= 1)
+        self.vending_machine = scene_elements.VendingMachine(self.area_vm,
+                                                             allow_overlapping=False,
+                                                             reward=1)
         self.add_scene_element(self.vending_machine)
 
         self.time_limit = 2000
 
     def assign_areas(self):
 
-        list_coord = [ (50, 50), (50, 150), (150,150), (150, 50) ]
+        list_coord = [(50, 50), (50, 150), (150, 150), (150, 50)]
         random.shuffle(list_coord)
 
         # Starting area of the agent
         area_start_center = list_coord.pop()
-        area_start = PositionAreaSampler(center=area_start_center, area_shape='rectangle',
+        area_start = PositionAreaSampler(center=area_start_center,
+                                         area_shape='rectangle',
                                          width_length=[80, 80])
 
         self.agent_starting_area = area_start
@@ -228,10 +253,12 @@ class CoinMaster(SingleRoom):
         vm_center = list_coord.pop()
         prod_center = list_coord.pop()
 
-        self.area_prod = PositionAreaSampler(center=prod_center, area_shape='rectangle', width_length=[80, 80])
-        self.area_vm = PositionAreaSampler(center=vm_center, area_shape='rectangle',
+        self.area_prod = PositionAreaSampler(center=prod_center,
+                                             area_shape='rectangle',
+                                             width_length=[80, 80])
+        self.area_vm = PositionAreaSampler(center=vm_center,
+                                           area_shape='rectangle',
                                            width_length=[80, 80])
-
 
     def reset(self):
         self.assign_areas()
@@ -248,5 +275,3 @@ class CoinMaster(SingleRoom):
 
         super()._fields_produce()
         self.vending_machine.accepted_coins = self.field.produced_entities
-
-
--- spg_experiments/wrappers/gym_env.py
+++ spg_experiments/wrappers/gym_env.py
@@ -1,127 +1,73 @@
+import random
+
 import gym
 import numpy
-from environments.rl import *
 from gym import spaces
 from simple_playgrounds import Engine
-from simple_playgrounds.controllers import External
-from simple_playgrounds.entities.agents import BaseAgent, BaseInteractiveAgent
-from simple_playgrounds.entities.agents.agent import Agent
-from simple_playgrounds.entities.agents.parts import (Arm, Hand, HolonomicPlatform)
-from simple_playgrounds.entities.agents.sensors import (DepthSensor, RgbSensor,
-                                                        TouchSensor)
+from simple_playgrounds.agents import agents, controllers, sensors
+from simple_playgrounds.agents.parts.platform import ForwardBackwardPlatform
+from simple_playgrounds.playgrounds import PlaygroundRegister
 from simple_playgrounds.utils import ActionTypes, SensorModality
-from stable_baselines.common import set_global_seeds
-
-
-class Agent_base(BaseInteractiveAgent):
-    def __init__(self, sensors):
-        super().__init__(controller=External(), allow_overlapping=False)
-
-        for sensor_name, sensor_params in sensors:
-
-            if sensor_name == 'depth':
-                self.add_sensor(
-                    DepthSensor(anchor=self.base_platform,
-                                normalize=True,
-                                **sensor_params))
-
-            elif sensor_name == 'rgb':
-                self.add_sensor(
-                    RgbSensor(anchor=self.base_platform, normalize=True, **sensor_params))
-
-            elif sensor_name == 'touch':
-                self.add_sensor(
-                    TouchSensor(anchor=self.base_platform,
-                                normalize=True,
-                                **sensor_params))
-
-
-import math
-
-
-class Agent_base_arm(Agent):
-    """
-        Agent with two Arms.
-
-        Attributes:
-            head: Head of the agent
-            arm_l: left arm
-            arm_l_2: second segment of left arm
-            arm_r: right arm
-            arm_r_2: second segment of right arm
-        """
-    def __init__(self, sensors):
-
-        base_agent = HolonomicPlatform(can_eat=False,
-                                       can_grasp=False,
-                                       can_activate=False,
-                                       can_absorb=False,
-                                       radius=8)
-
-        super().__init__(initial_position=None,
-                         base_platform=base_agent,
-                         controller=External(),
-                         allow_overlapping=False)
-
-        self.arm_r = Arm(base_agent, [8, 0],
-                         angle_offset=-math.pi / 2,
-                         rotation_range=math.pi,
-                         width_length=[3, 10])
-        self.add_body_part(self.arm_r)
-
-        self.arm_r_2 = Arm(self.arm_r,
-                           self.arm_r.extremity_anchor_point,
-                           angle_offset=math.pi / 2,
-                           rotation_range=math.pi,
-                           width_length=[3, 10])
-        self.add_body_part(self.arm_r_2)
-
-        self.hand_r = Hand(self.arm_r_2,
-                           self.arm_r_2.extremity_anchor_point,
-                           radius=5,
-                           rotation_range=math.pi,
-                           can_grasp=True)
-        self.add_body_part(self.hand_r)
-
-        for sensor_name, sensor_params in sensors:
-
-            if sensor_name == 'depth':
-                self.add_sensor(
-                    DepthSensor(anchor=self.base_platform,
-                                normalize=True,
-                                **sensor_params))
-
-            elif sensor_name == 'rgb':
-                self.add_sensor(
-                    RgbSensor(anchor=self.base_platform, normalize=True, **sensor_params))
-
-            elif sensor_name == 'touch':
-                self.add_sensor(
-                    TouchSensor(anchor=self.hand_r, normalize=True, **sensor_params))
 
 
 class PlaygroundEnv(gym.Env):
     """Custom Environment that follows gym interface"""
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, playground, agent, continuous_action_space=True, multisteps=None):
-        """
-        Args:
-            game_engine: Engine to run the
-
-        """
-
+    def __init__(self,
+                 playground_name,
+                 agent_type,
+                 sensors_name,
+                 seed=0,
+                 continuous_action_space=True,
+                 multisteps=None):
         super().__init__()
 
-        self.time_limit = playground.time_limit
+        self.playground = PlaygroundRegister.playgrounds[playground_name]()
 
-        self.game = Engine(playground, replay=False, screen=False)
+        seed = (seed + id(self)) % (2**32)
+        random.seed(seed)
+        numpy.random.seed(seed)
+
+        if agent_type == 'base':
+            agent_cls = agents.BaseAgent
+        elif agent_type == 'arm':
+            agent_cls = agents.FullAgent
+        else:
+            raise ValueError(f"Wrong agent_type: {agent_type}")
+
+        agent = agent_cls(platform=ForwardBackwardPlatform,
+                          controller=controllers.External())
+
+        for sensor_name, sensor_params in get_sensor_params(sensors_name):
+            if sensor_name == 'depth':
+                agent.add_sensor(
+                    sensors.Lidar(anchor=agent.base_platform,
+                                  normalize=True,
+                                  **sensor_params))
+
+            elif sensor_name == 'rgb':
+                agent.add_sensor(
+                    sensors.RgbCamera(anchor=agent.base_platform,
+                                      normalize=True,
+                                      **sensor_params))
+
+            elif sensor_name == 'touch':
+                agent.add_sensor(
+                    sensors.Touch(anchor=agent.base_platform,
+                                  normalize=True,
+                                  **sensor_params))
+
+        self.playground.add_agent(agent)
+        self.time_limit = self.playground.time_limit
+
+        self.game = Engine(self.playground, screen=False)
         self.agent = agent
         assert self.agent in self.game.agents
 
         # Define action space
 
-        actions = self.agent.get_all_available_actions()
+        actuators = self.agent.get_all_actuators()
         self.continuous_action_space = continuous_action_space
         self.actions_dict = {}
 
@@ -130,28 +76,22 @@ class PlaygroundEnv(gym.Env):
             lows = []
             highs = []
 
-            for action in actions:
+            for actuator in actuators:
 
-                if action.action_type is ActionTypes.DISCRETE:
+                if actuator.action_type is ActionTypes.DISCRETE:
                     lows.append(-1)
                     highs.append(1)
 
-                elif action.action_type is ActionTypes.CONTINUOUS_CENTERED:
-                    lows.append(action.min)
-                    highs.append(action.max)
+                elif actuator.action_type is ActionTypes.CONTINUOUS_CENTERED:
+                    lows.append(actuator.min)
+                    highs.append(actuator.max)
 
-                elif action.action_type is ActionTypes.CONTINUOUS_NOT_CENTERED:
-                    lows.append(action.min)
-                    highs.append(action.max)
+                elif actuator.action_type is ActionTypes.CONTINUOUS_NOT_CENTERED:
+                    lows.append(actuator.min)
+                    highs.append(actuator.max)
 
-                # lows.append(action.min)
-                # highs.append(action.max)
-
-                if action.body_part not in self.actions_dict:
-                    self.actions_dict[action.body_part] = {}
-
-                if action.action not in self.actions_dict[action.body_part]:
-                    self.actions_dict[action.body_part][action.action] = 0
+                lows.append(actuator.min)
+                highs.append(actuator.max)
 
             self.action_space = spaces.Box(low=numpy.array(lows), high=numpy.array(highs))
 
@@ -159,12 +99,10 @@ class PlaygroundEnv(gym.Env):
 
             dims = []
 
-            for action in actions:
-                if action.action_type is ActionTypes.DISCRETE:
+            for actuator in actuators:
+                if actuator.action_type is ActionTypes.DISCRETE:
                     dims.append(2)
-                elif action.action_type is ActionTypes.CONTINUOUS_CENTERED:
-                    dims.append(3)
-                elif action.action_type is ActionTypes.CONTINUOUS_NOT_CENTERED:
+                elif actuator.action_type is ActionTypes.CONTINUOUS_NOT_CENTERED:
                     dims.append(2)
 
             self.action_space = spaces.MultiDiscrete(dims)
@@ -204,49 +142,50 @@ class PlaygroundEnv(gym.Env):
             assert isinstance(multisteps, int)
             self.multisteps = multisteps
 
+    @property
+    def engine(self):
+        return self.game
+
     def get_current_timestep(self):
         return self.game.elapsed_time
 
-    def step(self, action):
+    def step(self, actions):
 
         # First, send actions to game engint
 
         actions_to_game_engine = {}
+        actions_dict = {}
 
         # Convert Stable-baselines actions into game engine actions
-        for index_action, available_action in enumerate(
-                self.agent.get_all_available_actions()):
-
-            body_part = available_action.body_part
-            action_ = available_action.action
-            action_type = available_action.action_type
-
-            converted_action = action[index_action]
+        for actuator, action in zip(self.agent.get_all_actuators(), actions):
+            action_type = actuator.action_type
+            converted_action = action
 
             # convert discrete action to binry
             if self.continuous_action_space and action_type is ActionTypes.DISCRETE:
                 converted_action = 0 if converted_action < 0 else 1
 
             # convert continuous actions in [-1, 1]
-            elif not self.continuous_action_space and action_type is ActionTypes.CONTINUOUS_CENTERED:
+            elif (not self.continuous_action_space) and (action_type is
+                                                         ActionTypes.CONTINUOUS_CENTERED):
                 converted_action = converted_action - 1
 
-            self.actions_dict[body_part][action_] = converted_action
+            actions_dict[actuator] = converted_action
 
-        actions_to_game_engine[self.agent.name] = self.actions_dict
+        actions_to_game_engine[self.agent] = actions_dict
 
         # Generate actions for other agents
         for agent in self.game.agents:
             if agent is not self.agent:
-                actions_to_game_engine[agent.name] = agent.controller.generate_actions()
+                actions_to_game_engine[agent] = agent.controller.generate_actions()
 
         # Now that we have all ctions, run the engine, and get the observations
 
         if self.multisteps is None:
-            reset, terminate = self.game.step(actions_to_game_engine)
+            terminate = self.game.step(actions_to_game_engine)
         else:
-            reset, terminate = self.game.multiple_steps(actions_to_game_engine,
-                                                        n_steps=self.multisteps)
+            terminate = self.game.multiple_steps(actions_to_game_engine,
+                                                 n_steps=self.multisteps)
 
         self.game.update_observations()
 
@@ -262,21 +201,16 @@ class PlaygroundEnv(gym.Env):
 
             elif len(sensor.shape) == 2:
                 self.observations[
-                    current_height, :sensor.shape[0], :] = sensor.sensor_value[:, :]
+                    current_height, :sensor.shape[0], :] = sensor.sensor_values[:, :]
                 current_height += 1
 
             else:
                 self.observations[:sensor.shape[0], :sensor.
-                                  shape[1], :] = sensor.sensor_value[:, :, :]
+                                  shape[1], :] = sensor.sensor_values[:, :, :]
                 current_height += sensor.shape[0]
 
         reward = self.agent.reward
-
-        if reset or terminate:
-            done = True
-
-        else:
-            done = False
+        done = self.playground.done or terminate
 
         return (self.observations, reward, done, {})
 
@@ -288,47 +222,52 @@ class PlaygroundEnv(gym.Env):
         return numpy.zeros(self.observations.shape)
 
     def render(self, mode='human'):
-        img = self.game.generate_topdown_image()
+        img = self.game.generate_playground_image()
         return img
 
     def close(self):
         self.game.terminate()
 
 
-import random
+def get_sensor_params(sensors_name):
+    if sensors_name == 'rgb':
+        sensors = [('rgb', {'fov': 180, 'range': 300, 'resolution': 64})]
 
+    elif sensors_name == 'rgb_depth':
+        sensors = [('depth', {
+            'fov': 180,
+            'range': 300,
+            'resolution': 64
+        }), ('rgb', {
+            'fov': 180,
+            'range': 300,
+            'resolution': 64
+        })]
 
-def make_vector_env(playground_name, agent_type, sensors, rank, multisteps=None, seed=0):
-    """
-    Utility function for multiprocessed env.
+    elif sensors_name == 'rgb_touch':
+        sensors = [('rgb', {
+            'fov': 180,
+            'range': 300,
+            'resolution': 64
+        }), ('touch', {
+            'range': 2,
+            'resolution': 64
+        })]
 
-    Args:
-        pg: Instance of a Playground
-        Ag: Agent
-    """
-    def _init():
+    elif sensors_name == 'rgb_depth_touch':
+        sensors = [('depth', {
+            'fov': 180,
+            'range': 300,
+            'resolution': 64
+        }), ('rgb', {
+            'fov': 180,
+            'range': 300,
+            'resolution': 64
+        }), ('touch', {
+            'range': 2,
+            'resolution': 64
+        })]
+    else:
+        raise ValueError(f"Wrong sensors_name: {sensors_name}")
 
-        random.seed(seed)
-        numpy.random.seed(seed)
-
-        playground = PlaygroundRegister.playgrounds[playground_name]()
-
-        random.seed(seed + rank)
-        numpy.random.seed(seed + rank)
-
-        if agent_type == 'base':
-            agent = Agent_base(sensors)
-
-        elif agent_type == 'arm':
-            agent = Agent_base_arm(sensors)
-
-        playground.add_agent(agent)
-
-        custom_env = PlaygroundEnv(playground,
-                                   agent,
-                                   multisteps=multisteps,
-                                   continuous_action_space=True)
-
-        return custom_env
-
-    return _init
+    return sensors
