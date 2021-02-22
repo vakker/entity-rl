355d5db2e5b67dcb2d9995d47c69942ac18e900e
Vince Jankovics <vincejankovics@gmail.com>
2021-02-22 13:09:22 +0000

    Configs worked out

--- ray_exp.py
+++ ray_exp.py
@@ -1,4 +1,5 @@
 import argparse
+from datetime import datetime
 
 import ray
 from ray import tune
@@ -20,9 +21,13 @@ parser.add_argument("--local", action="store_true")
 parser.add_argument("--gpu", action="store_true")
 parser.add_argument("--monitor", action="store_true")
 parser.add_argument("--stop-iters", type=int, default=50)
-parser.add_argument("--num-workers", type=int, default=10)
+parser.add_argument("--num-workers", type=int, default=5)
 parser.add_argument("--stop-timesteps", type=int, default=1000000)
 parser.add_argument("--stop-reward", type=float, default=75)
+
+
+def exp_name(prefix):
+    return prefix + '.' + datetime.now().strftime("%Y-%m-%d.%H:%M:%S")
 
 
 class E(dict):
@@ -51,7 +56,7 @@ def main(args):
         # "record_env": args.monitor,
         "env_config": {
             "agent_type": "base",
-            "index_exp": grid_search([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            # "index_exp": grid_search([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
             "playground_name": grid_search([
                 "spg_endgoal_cue",
                 "spg_endgoal_9rooms",
@@ -60,35 +65,36 @@ def main(args):
             ]),
             "sensors_name": grid_search(["rgb", "rgb_depth rgb_touch",
                                          "rgb_depth_touch"]),
-            "entropy": grid_search([0.05, 0.01, 0.005, 0.001]),
             "multistep": ([0, 2, 3, 4])
         },
         # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-        "num_gpus": 1 if args.gpu else 0,
+        "num_gpus": 0.5 if args.gpu else 0,
         # "model": {
         #     "custom_model": "my_model",
         #     "vf_share_layers": True,
         #     "fcnet_hiddens": [1],
         # },
-        "lr": 1e-5,
         # # "lr": grid_search([1e-2, 1e-4, 1e-6]),  # try different lrs
         "framework": "torch",
-        "lambda": 0.95,
-        "kl_coeff": 0.5,
-        "clip_rewards": True,
-        "clip_param": 0.1,
-        "vf_clip_param": 10.0,
-        "vf_loss_coeff": 0.01,
-        "entropy_coeff": 0.01,
+        "gamma": 0.99,  # checked
+        "lr": 0.00025,  # checked
+        "lambda": 0.95,  # checked
+        "kl_coeff": 0.5,  # ?
+        "clip_rewards": False,  # checked?
+        "clip_param": 0.2,  # checked?
+        "grad_clip": 0.5,  # checked
+        "vf_clip_param": 10000,  # checked, it's None in SB
+        "vf_loss_coeff": 0.5,  # checked
+        "entropy_coeff": grid_search([0.05, 0.01, 0.005, 0.001]),  # checked
         # "timesteps_per_iteration": 500,
         # "train_batch_size": 500,
         # "sgd_minibatch_size": 100,
-        # "num_sgd_iter": 1,
+        "num_sgd_iter": 4,  # checked
         "num_envs_per_worker": 1,
-        "timesteps_per_iteration": 5000,
-        "train_batch_size": 5000,
-        "sgd_minibatch_size": 1000,
-        "num_sgd_iter": 10,
+        # "timesteps_per_iteration": 128 * 4,  # this seems redundant
+        "train_batch_size": 128 * 4,  # checked, but check the *4
+        "sgd_minibatch_size": 128,  # train_batch_size/4
+        "num_sgd_iter": 4,  # checked, not sure
         # "num_envs_per_worker": 5,
         "batch_mode": "truncate_episodes",
         "observation_filter": "NoFilter",
@@ -113,6 +119,7 @@ def main(args):
         # "episode_reward_mean": args.stop_reward,
     }
 
+    name = exp_name('PPO')
     reporter = CLIReporter(parameter_columns=E({"_": "_"}))
     results = tune.run(
         args.run,
@@ -124,6 +131,7 @@ def main(args):
         trial_name_creator=trial_str_creator,
         trial_dirname_creator=trial_str_creator,
         progress_reporter=reporter,
+        name=name,
         # max_failures=1,
         fail_fast=True)
 
