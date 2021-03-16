2d02022b551c15c72d240c33c082cc74f5adf790
Vince Jankovics <vincejankovics@gmail.com>
2021-03-16 12:01:29 +0000

    Custom vision 1d added

--- spg_experiments/wrappers/models.py
+++ spg_experiments/wrappers/models.py
@@ -1,8 +1,49 @@
+from typing import Any, Dict, List, Tuple, Union
+
+import gym
+import numpy as np
 import torch
 from ray.rllib.models import ModelCatalog
 from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
+from ray.rllib.models.torch.misc import SlimFC, normc_initializer
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.models.utils import get_activation_fn, get_filter_config
+from ray.rllib.utils.annotations import override
+from ray.rllib.utils.typing import ModelConfigDict, TensorType
 from torch import nn
+
+
+def same_padding_1d(
+    in_size: int,
+    filter_size: int,
+    stride_size: int,
+) -> (int, Tuple[int, int]):
+    """Note: Padding is added to match TF conv2d `same` padding. See
+    www.tensorflow.org/versions/r0.12/api_docs/python/nn/convolution
+
+    Args:
+        in_size (tuple): Rows (Height), Column (Width) for input
+        stride_size (Union[int,Tuple[int, int]]): Rows (Height), column (Width)
+            for stride. If int, height == width.
+        filter_size (tuple): Rows (Height), column (Width) for filter
+
+    Returns:
+        padding (tuple): For input into torch.nn.ZeroPad2d.
+        output (tuple): Output shape after padding and convolution.
+    """
+    in_width = in_size
+    filter_width = filter_size
+    stride_width = stride_size
+
+    out_width = np.ceil(float(in_width) / float(stride_width))
+
+    pad_along_width = int(((out_width - 1) * stride_width + filter_width - in_width))
+    pad_left = pad_along_width // 2
+    pad_right = pad_along_width - pad_left
+    padding = (pad_left, pad_right)
+    output = out_width
+    return padding, output
+
 
 # def ortho_init_1d(scale=1.0):
 #     """
@@ -98,100 +139,336 @@ class CustomFC(TorchModelV2, nn.Module):
     def value_function(self):
         return torch.reshape(self.torch_sub_model.value_function(), [-1])
 
-    #     activ = 'relu'
 
-    #     self.custom_layers = []
-    #     self.observation_shape = _kwargs['observation_shape']
+class SlimConv1d(nn.Module):
+    """Simple mock of tf.slim Conv2d"""
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            kernel: int,
+            stride: int,
+            padding: Tuple[int, int],
+            # Defaulting these to nn.[..] will break soft torch import.
+            initializer: Any = "default",
+            activation_fn: Any = "default",
+            bias_init: float = 0):
+        """Creates a standard Conv2d layer, similar to torch.nn.Conv2d
 
-    #     self.reuse = reuse
+            Args:
+                in_channels(int): Number of input channels
+                out_channels (int): Number of output channels
+                kernel (Union[int, Tuple[int, int]]): If int, the kernel is
+                    a tuple(x,x). Elsewise, the tuple can be specified
+                stride (Union[int, Tuple[int, int]]): Controls the stride
+                    for the cross-correlation. If int, the stride is a
+                    tuple(x,x). Elsewise, the tuple can be specified
+                padding (Union[int, Tuple[int, int]]): Controls the amount
+                    of implicit zero-paddings during the conv operation
+                initializer (Any): Initializer function for kernel weights
+                activation_fn (Any): Activation function at the end of layer
+                bias_init (float): Initalize bias weights to bias_init const
+        """
+        super().__init__()
+        layers = []
+        # Padding layer.
+        if padding:
+            layers.append(nn.ConstantPad1d(padding, 0))
+        # Actual Conv1D layer (including correct initialization logic).
+        conv = nn.Conv1d(in_channels, out_channels, kernel, stride)
+        if initializer:
+            if initializer == "default":
+                initializer = nn.init.xavier_uniform_
+            initializer(conv.weight)
+        nn.init.constant_(conv.bias, bias_init)
+        layers.append(conv)
+        # Activation function (if any; default=ReLu).
+        if isinstance(activation_fn, str):
+            if activation_fn == "default":
+                activation_fn = nn.ReLU
+            else:
+                activation_fn = get_activation_fn(activation_fn, "torch")
+        if activation_fn is not None:
+            layers.append(activation_fn())
+        # Put everything in sequence.
+        self._model = nn.Sequential(*layers)
 
-    #     self._setup_init()
+    def forward(self, x: TensorType) -> TensorType:
+        if x.dim() == 4:
+            x = torch.squeeze(x, dim=2)
+        return self._model(x)
 
-    # def feature_extractor(self, input_observations, **kwargs):
 
-    #     activ = tf.nn.relu
-    #     activ_dense = tf.nn.relu
+class VisionNetwork1D(TorchModelV2, nn.Module):
+    """Generic vision network."""
+    def __init__(self, obs_space: gym.spaces.Space, action_space: gym.spaces.Space,
+                 num_outputs: int, model_config: ModelConfigDict, name: str):
+        if not model_config.get("conv_filters"):
+            model_config["conv_filters"] = get_filter_config(obs_space.shape)
+        TorchModelV2.__init__(self, obs_space, action_space, num_outputs, model_config,
+                              name)
+        nn.Module.__init__(self)
 
-    #     current_height = 0
+        activation = self.model_config.get("conv_activation")
+        filters = self.model_config["conv_filters"]
+        assert len(filters) > 0,\
+            "Must provide at least 1 entry in `conv_filters`!"
+        no_final_linear = self.model_config.get("no_final_linear")
+        vf_share_layers = self.model_config.get("vf_share_layers")
 
-    #     features = []
+        # Whether the last layer is the output of a Flattened (rather than
+        # a n x (1,1) Conv2D).
+        self.last_layer_is_flattened = False
+        self._logits = None
 
-    #     # if input_observations.shape[0] != 128:
-    #     #     print(input_observations.shape)
+        layers = []
+        (h, w, in_channels) = obs_space.shape
+        assert h == 1
+        in_size = w
+        for out_channels, kernel, stride in filters[:-1]:
+            padding, out_size = same_padding_1d(in_size, kernel, stride)
+            layers.append(
+                SlimConv1d(in_channels,
+                           out_channels,
+                           kernel,
+                           stride,
+                           padding,
+                           activation_fn=activation))
+            in_channels = out_channels
+            in_size = out_size
 
-    #     with tf.variable_scope("model", reuse=self.reuse):
+        out_channels, kernel, stride = filters[-1]
 
-    #         for index_observation, shape in enumerate(self.observation_shape):
+        # No final linear: Last layer is a Conv2D and uses num_outputs.
+        if no_final_linear and num_outputs:
+            layers.append(
+                SlimConv1d(
+                    in_channels,
+                    num_outputs,
+                    kernel,
+                    stride,
+                    None,  # padding=valid
+                    activation_fn=activation))
+            out_channels = num_outputs
+        # Finish network normally (w/o overriding last layer size with
+        # `num_outputs`), then add another linear one of size `num_outputs`.
+        else:
+            layers.append(
+                SlimConv1d(
+                    in_channels,
+                    out_channels,
+                    kernel,
+                    stride,
+                    None,  # padding=valid
+                    activation_fn=activation))
 
-    #             height, width, channel = shape
+            # num_outputs defined. Use that to create an exact
+            # `num_output`-sized (1,1)-Conv2D.
+            if num_outputs:
+                in_size = np.ceil((in_size - kernel) / stride)
 
-    #             # Get observation
+                padding, _ = same_padding_1d(in_size, 1, 1)
+                self._logits = SlimConv1d(out_channels,
+                                          num_outputs,
+                                          1,
+                                          1,
+                                          padding,
+                                          activation_fn=None)
+            # num_outputs not known -> Flatten, then set self.num_outputs
+            # to the resulting number of nodes.
+            else:
+                self.last_layer_is_flattened = True
+                layers.append(nn.Flatten())
+                self.num_outputs = out_channels
 
-    #             obs = input_observations[:, current_height:current_height +
-    #                                      height, :width, :channel]
+        self._convs = nn.Sequential(*layers)
 
-    #             if height == 1:
-    #                 obs = tf.squeeze(obs, axis=[1])
-    #             current_height += height
+        # Build the value layers
+        self._value_branch_separate = self._value_branch = None
+        if vf_share_layers:
+            self._value_branch = SlimFC(out_channels,
+                                        1,
+                                        initializer=normc_initializer(0.01),
+                                        activation_fn=None)
+        else:
+            vf_layers = []
+            (h, w, in_channels) = obs_space.shape
+            assert h == 1
+            in_size = w
+            for out_channels, kernel, stride in filters[:-1]:
+                padding, out_size = same_padding_1d(in_size, kernel, stride)
+                vf_layers.append(
+                    SlimConv1d(in_channels,
+                               out_channels,
+                               kernel,
+                               stride,
+                               padding,
+                               activation_fn=activation))
+                in_channels = out_channels
+                in_size = out_size
 
-    #             layer_1 = activ(
-    #                 conv_1d(obs,
-    #                         'c1_' + str(index_observation),
-    #                         n_filters=64,
-    #                         filter_size=5,
-    #                         stride=3,
-    #                         init_scale=numpy.sqrt(2)))
+            out_channels, kernel, stride = filters[-1]
+            vf_layers.append(
+                SlimConv1d(in_channels,
+                           out_channels,
+                           kernel,
+                           stride,
+                           None,
+                           activation_fn=activation))
 
-    #             # layer_1 = tf.nn.dropout( layer_1, rate = 0.2)
-    #             # layer_1 = tf.layers.batch_normalization(layer_1)
+            vf_layers.append(
+                SlimConv1d(in_channels=out_channels,
+                           out_channels=1,
+                           kernel=1,
+                           stride=1,
+                           padding=None,
+                           activation_fn=None))
+            self._value_branch_separate = nn.Sequential(*vf_layers)
 
-    #             layer_2 = activ(
-    #                 conv_1d(layer_1,
-    #                         'c2_' + str(index_observation),
-    #                         n_filters=64,
-    #                         filter_size=3,
-    #                         stride=2,
-    #                         init_scale=numpy.sqrt(2)))
+        # Holds the current "base" output (before logits layer).
+        self._features = None
 
-    #             # layer_2 = tf.nn.dropout(layer_2, rate = 0.2)
-    #             # layer_2 = tf.layers.batch_normalization(layer_2)
+    @override(TorchModelV2)
+    def forward(self, input_dict: Dict[str, TensorType], state: List[TensorType],
+                seq_lens: TensorType) -> (TensorType, List[TensorType]):
+        self._features = input_dict["obs"].float().permute(0, 3, 1, 2)
+        conv_out = self._convs(self._features)
+        # Store features to save forward pass when getting value_function out.
+        if not self._value_branch_separate:
+            self._features = conv_out
 
-    #             # layer_3 = activ(
-    #             #     conv_1d(layer_2, 'c3_' + str(index_observation), n_filters=64, filter_size=3, stride=1,
-    #             #             init_scale=numpy.sqrt(2)))
+        if not self.last_layer_is_flattened:
+            if self._logits:
+                conv_out = self._logits(conv_out)
+            if conv_out.shape[2] != 1:
+                raise ValueError("Given `conv_filters` ({}) do not result in a [B, {} "
+                                 "(`num_outputs`), 1] shape (but in {})! Please adjust "
+                                 "your Conv1D stack such that the last dim is "
+                                 "1.".format(self.model_config["conv_filters"],
+                                             self.num_outputs, list(conv_out.shape)))
+            logits = conv_out.squeeze(2)
+            # logits = logits.squeeze(1)
 
-    #             # layer_3 = tf.nn.dropout(layer_3, rate = 0.3)
-    #             # layer_3 = tf.layers.batch_normalization(layer_3)
+            return logits, state
+        else:
+            return conv_out, state
 
-    #             layer_3 = conv_to_fc(layer_2)
+    @override(TorchModelV2)
+    def value_function(self) -> TensorType:
+        assert self._features is not None, "must call forward() first"
+        if self._value_branch_separate:
+            value = self._value_branch_separate(self._features)
+            value = value.squeeze(2)
+            value = value.squeeze(1)
+            return value
+        else:
+            if not self.last_layer_is_flattened:
+                features = self._features.squeeze(2)
+                # features = features.squeeze(2)
+            else:
+                features = self._features
+            return self._value_branch(features).squeeze(1)
 
-    #             dense_1 = activ_dense(
-    #                 linear(layer_3,
-    #                        'fc1_' + str(index_observation),
-    #                        n_hidden=128,
-    #                        init_scale=numpy.sqrt(2)))
-    #             # dense_1 = tf.nn.dropout(dense_1, rate = 0.2)
-    #             # dense_1 = tf.layers.batch_normalization(dense_1)
+    def _hidden_layers(self, obs: TensorType) -> TensorType:
+        res = self._convs(obs.permute(0, 3, 1, 2))  # switch to channel-major
+        # res = res.squeeze(3)
+        res = res.squeeze(2)
+        return res
 
-    #             # dense_2 = activ(linear(dense_1, 'fc2_'+ str(index_observation), n_hidden=128, init_scale=numpy.sqrt(2)))
-    #             # dense_2 = tf.nn.dropout(dense_2, rate = 0.3)
-    #             # dense_2 = tf.layers.batch_normalization(dense_2)
 
-    #             features.append(dense_1)
+# class CustomCNN(TorchModelV2, nn.Module):
+#     def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+#         TorchModelV2.__init__(self, obs_space, action_space, num_outputs, model_config,
+#                               name)
+#         nn.Module.__init__(self)
 
-    #         h_concat = tf.concat(features, 1)
+#         self.activ = 'ReLU'
 
-    #         h_out_1 = activ_dense(
-    #             linear(h_concat, 'dense_1', n_hidden=128, init_scale=numpy.sqrt(2)))
-    #         # h_out_1 = tf.nn.dropout(h_out_1, rate = 0.2)
-    #         # h_out_1 = tf.layers.batch_normalization(h_out_1)
-    #         #
-    #         # h_out_2 = activ(linear(h_out_1, 'dense_2', n_hidden=128, init_scale=numpy.sqrt(2)))
-    #         # h_out_2 = tf.nn.dropout(h_out_2, rate = 0.2)
-    #         # h_out_2 = tf.layers.batch_normalization(h_out_2)
+#         self.custom_layers = []
+#         self.observation_shape = obs_space
 
-    #     return h_out_1
+#     def feature_extractor(self, input_observations, **kwargs):
 
+#         activ = getattr(nn, self.activ)
+#         activ_dense = getattr(nn, self.activ)
+
+#         current_height = 0
+
+#         features = []
+
+#         # if input_observations.shape[0] != 128:
+#         #     print(input_observations.shape)
+
+#         for index_observation, shape in enumerate(self.observation_shape):
+
+#             height, width, channel = shape
+
+#             # Get observation
+
+#             obs = input_observations[:, current_height:current_height +
+#                                      height, :width, :channel]
+
+#             if height == 1:
+#                 obs = tf.squeeze(obs, axis=[1])
+#             current_height += height
+
+#             layer_1 = activ(
+#                 conv_1d(obs,
+#                         'c1_' + str(index_observation),
+#                         n_filters=64,
+#                         filter_size=5,
+#                         stride=3,
+#                         init_scale=numpy.sqrt(2)))
+
+#             # layer_1 = tf.nn.dropout( layer_1, rate = 0.2)
+#             # layer_1 = tf.layers.batch_normalization(layer_1)
+
+#             layer_2 = activ(
+#                 conv_1d(layer_1,
+#                         'c2_' + str(index_observation),
+#                         n_filters=64,
+#                         filter_size=3,
+#                         stride=2,
+#                         init_scale=numpy.sqrt(2)))
+
+#             # layer_2 = tf.nn.dropout(layer_2, rate = 0.2)
+#             # layer_2 = tf.layers.batch_normalization(layer_2)
+
+#             # layer_3 = activ(
+#             #     conv_1d(layer_2, 'c3_' + str(index_observation), n_filters=64, filter_size=3, stride=1,
+#             #             init_scale=numpy.sqrt(2)))
+
+#             # layer_3 = tf.nn.dropout(layer_3, rate = 0.3)
+#             # layer_3 = tf.layers.batch_normalization(layer_3)
+
+#             layer_3 = conv_to_fc(layer_2)
+
+#             dense_1 = activ_dense(
+#                 linear(layer_3,
+#                        'fc1_' + str(index_observation),
+#                        n_hidden=128,
+#                        init_scale=numpy.sqrt(2)))
+#             # dense_1 = tf.nn.dropout(dense_1, rate = 0.2)
+#             # dense_1 = tf.layers.batch_normalization(dense_1)
+
+#             # dense_2 = activ(linear(dense_1, 'fc2_'+ str(index_observation), n_hidden=128, init_scale=numpy.sqrt(2)))
+#             # dense_2 = tf.nn.dropout(dense_2, rate = 0.3)
+#             # dense_2 = tf.layers.batch_normalization(dense_2)
+
+#             features.append(dense_1)
+
+#         h_concat = tf.concat(features, 1)
+
+#         h_out_1 = activ_dense(
+#             linear(h_concat, 'dense_1', n_hidden=128, init_scale=numpy.sqrt(2)))
+#         # h_out_1 = tf.nn.dropout(h_out_1, rate = 0.2)
+#         # h_out_1 = tf.layers.batch_normalization(h_out_1)
+#         #
+#         # h_out_2 = activ(linear(h_out_1, 'dense_2', n_hidden=128, init_scale=numpy.sqrt(2)))
+#         # h_out_2 = tf.nn.dropout(h_out_2, rate = 0.2)
+#         # h_out_2 = tf.layers.batch_normalization(h_out_2)
+
+#         return h_out_1
 
 # import os
 # from typing import Any, Dict, List, Optional, Union
@@ -288,3 +565,4 @@ class CustomFC(TorchModelV2, nn.Module):
 #         return True
 
 ModelCatalog.register_custom_model("custom-fc", CustomFC)
+ModelCatalog.register_custom_model("vision-1d", VisionNetwork1D)
