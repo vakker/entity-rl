954c5f528ba0787a9a4a6c3446801b03dc64e559
Vince Jankovics <vincejankovics@gmail.com>
2021-12-09 10:11:31 +0000

    Commit old change

--- ray_exp.py
+++ ray_exp.py
@@ -7,7 +7,7 @@ from ray.tune import CLIReporter
 from ray.tune.suggest.variant_generator import grid_search
 
 from spg_experiments import models
-from spg_experiments.gym_env import PlaygroundEnv
+from spg_experiments.gym_env import PlaygroundEnv, PlaygroundEnvSemantic
 
 
 def exp_name(prefix):
@@ -20,11 +20,11 @@ class E(dict):
 
 
 def trial_str_creator(trial):
-    name = '-'.join([
-        p[-1] if isinstance(p, list) else str(p)
+    params = {
+        k.split('/')[-1]: p[-1] if isinstance(p, list) else str(p)
         for k, p in trial.evaluated_params.items()
-    ])
-    # return f'trial-{trial.trial_id}'
+    }
+    name = '-'.join([f'{k}:{p}' for k, p in params.items()])
     return f'trial-{name}'
 
 
@@ -35,28 +35,31 @@ def main(args):
         "num_workers": args.num_workers,  # parallelism
         "num_envs_per_worker": 2,
         "num_cpus_per_worker": 0.5,
-        "evaluation_num_workers": args.num_workers,
+        "evaluation_num_workers": args.num_workers
+        if args.evaluation_interval else 0,
         # "evaluation_config": {
         # },
-        "evaluation_interval": 10,
-        "env": PlaygroundEnv,
+        # "evaluation_interval": 10,
+        "evaluation_interval": args.evaluation_interval,
+        "env": PlaygroundEnvSemantic,
         "output": "logdir",
         "env_config": {
             "agent_type": "base",
             # "index_exp": grid_search([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
             "playground_name": grid_search([
-                ["foraging", "candy_collect"],
-                ["foraging", "candy_fireballs"],
-                ['navigation', 'endgoal_cue'],
-                ['sequential', 'door_dispenser_coin'],
+                ["foraging", "candy_poison"],
+                # ["foraging", "candy_fireballs"],
+                # ['navigation', 'endgoal_cue'],
+                # ['sequential', 'door_dispenser_coin'],
             ]),
             "sensors_name": grid_search([
-                "blind",
-                "rgb",
-                "depth",
-                "rgb_depth",
-                "rgb_touch",
-                "rgb_depth_touch",
+                "semantic"
+                # "blind",
+                # "rgb",
+                # "depth",
+                # "rgb_depth",
+                # "rgb_touch",
+                # "rgb_depth_touch",
             ]),
             # "multisteps": grid_search([0, 2, 3, 4])
             # "multisteps": 0
@@ -65,29 +68,37 @@ def main(args):
         "framework": "torch",
         # "gamma": grid_search([0.1, 0.2, 0.5, 0.8, 0.99]),  # checked
         # "lr": grid_search([0.001, 0.0001, 0.00001]),
+        "lr": 0.001,
         "lambda": 0.95,  # checked
         # "kl_coeff": 0.5,  # ?
         "clip_rewards": False,
         "clip_param": 0.2,  # checked?
-        "grad_clip": 0.5,  # checked
+        # "grad_clip": 0.5,  # checked
+        "grad_clip": 10,  # checked
         # "vf_clip_param": 10,  # checked, it's None in SB, 10 in RLlib
         "vf_loss_coeff": 0.0001,  # checked
         # "entropy_coeff": grid_search([0.05, 0.01, 0.005, 0.001]),  # checked
-        "train_batch_size": 128 * 10 * 8,  # checked, but check the *4*2
-        "sgd_minibatch_size": 128,  # could be larger
+        # "train_batch_size": 128 * 10 * 8,  # checked, but check the *4*2
+        "train_batch_size": 8,  # checked, but check the *4*2
+        # "sgd_minibatch_size": 128,  # could be larger
+        "sgd_minibatch_size": 8,  # could be larger
         "num_sgd_iter": 4,  # checked?
         "batch_mode": "truncate_episodes",
         "observation_filter": "NoFilter",
         "model": {
-            "custom_model": "vision-1d",
-            "conv_filters": [
-                [64, 5, 3],
-                [64, 3, 2],
-                [64, 3, 2],
-                [128, 3, 2],
-                [128, 3, 2],
-                # [128, 3, 2],
-            ],
+            "custom_model": "graph",
+            # "custom_model": "semantic",
+            # "custom_model": "vision-1d",
+            # "conv_filters": [
+            #     [64, 5, 3],
+            #     [64, 3, 2],
+            #     [64, 3, 2],
+            #     [128, 3, 2],
+            #     [128, 3, 2],
+            #     # [128, 3, 2],
+            # ],
+            "fcnet_hiddens": [64, 64, 64],
+            # "use_lstm": grid_search([True, False]),
         },
     }
 
@@ -128,6 +139,7 @@ if __name__ == "__main__":
     parser.add_argument("--stop-timesteps", type=int, default=1000000)
     parser.add_argument("--stop-iters", type=int)
     parser.add_argument("--stop-reward", type=float)
+    parser.add_argument("--evaluation-interval", type=int)
 
     args = parser.parse_args()
     main(args)
