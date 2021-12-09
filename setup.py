0ca6271beec330f7096fe2b8535417c8988e2737
Vince Jankovics <vincejankovics@gmail.com>
2021-12-09 15:23:41 +0000

    Workflow added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="spg-experiments",
+    version="0.0.1",
+    description="Description of my package",
+    packages=find_packages(),
+    install_requires=[],
+)
