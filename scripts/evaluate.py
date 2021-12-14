8367279c2acd92bc19446758e1861ee1234683a6
Vince Jankovics <vincejankovics@gmail.com>
2021-12-14 10:55:37 +0000

    Evan cleanup

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,24 +1,8 @@
-from ray.rllib import rollout
+from ray.rllib import evaluate
 
 from spg_experiments import utils
 
 if __name__ == "__main__":
-    parser = rollout.create_parser()
-    args = parser.parse_args()
-
     utils.register()
 
-    # --use_shelve w/o --out option.
-    if args.use_shelve and not args.out:
-        raise ValueError(
-            "If you set --use-shelve, you must provide an output file via "
-            "--out as well!"
-        )
-    # --track-progress w/o --out option.
-    if args.track_progress and not args.out:
-        raise ValueError(
-            "If you set --track-progress, you must provide an output file via "
-            "--out as well!"
-        )
-
-    rollout.run(args, parser)
+    evaluate.main()
