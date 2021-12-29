e509e455d760f390a9a6d385fc3572f2979097d4
Vince Jankovics <vincejankovics@gmail.com>
2021-12-29 19:21:23 +0000

    Graph env added

--- spg_experiments/envs/__init__.py
+++ spg_experiments/envs/__init__.py
@@ -1,4 +1,5 @@
 from .base import PgDict, PgFlat, PgStacked
+from .env_graph import PgGraph
 from .env_set import PgSet
 
-__all__ = ["PgFlat", "PgDict", "PgStacked", "PgSet"]
+__all__ = ["PgFlat", "PgDict", "PgStacked", "PgSet", "PgGraph"]
