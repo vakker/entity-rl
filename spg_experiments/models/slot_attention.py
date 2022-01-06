a51386a429de3ceaab92705ae56a326f4ab06d92
Vince Jankovics <vincejankovics@gmail.com>
2022-01-06 17:48:21 +0000

    SA tests and fixes

--- spg_experiments/models/slot_attention.py
+++ spg_experiments/models/slot_attention.py
@@ -7,77 +7,7 @@ from torch_scatter import scatter
 
 
 class SlotAttentionRef(nn.Module):
-    # pylint: disable=too-many-locals
-
-    def __init__(self, num_slots, dim, iters=3, eps=1e-8, hidden_dim=128):
-        super().__init__()
-        self.num_slots = num_slots
-        self.iters = iters
-        self.eps = eps
-        self.scale = dim ** -0.5
-
-        self.slots_mu = nn.Parameter(torch.randn(1, 1, dim))
-
-        self.slots_logsigma = nn.Parameter(torch.zeros(1, 1, dim))
-        init.xavier_uniform_(self.slots_logsigma)
-
-        self.to_q = nn.Linear(dim, dim)
-        self.to_k = nn.Linear(dim, dim)
-        self.to_v = nn.Linear(dim, dim)
-
-        self.gru = nn.GRUCell(dim, dim)
-
-        hidden_dim = max(dim, hidden_dim)
-
-        self.mlp = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.ReLU(inplace=True),
-            nn.Linear(hidden_dim, dim),
-        )
-
-        self.norm_input = nn.LayerNorm(dim)
-        self.norm_slots = nn.LayerNorm(dim)
-        self.norm_pre_ff = nn.LayerNorm(dim)
-
-    def forward(self, inputs, num_slots=None):
-        batch_size, _, n_input_features = inputs.shape
-
-        n_s = num_slots if num_slots is not None else self.num_slots
-
-        mu = self.slots_mu.expand(batch_size, n_s, -1)
-        sigma = self.slots_logsigma.exp().expand(batch_size, n_s, -1)
-
-        slots = mu + sigma * torch.randn(mu.shape, device=inputs.device)
-
-        inputs = self.norm_input(inputs)
-        k = self.to_k(inputs)
-        v = self.to_v(inputs)
-
-        for _ in range(self.iters):
-            slots_prev = slots
-
-            slots = self.norm_slots(slots)
-            q = self.to_q(slots)
-
-            dots = torch.einsum("bid,bjd->bij", q, k) * self.scale
-            attn = dots.softmax(dim=1) + self.eps
-            attn = attn / attn.sum(dim=-1, keepdim=True)
-
-            updates = torch.einsum("bjd,bij->bid", v, attn)
-
-            slots = self.gru(
-                updates.reshape(-1, n_input_features),
-                slots_prev.reshape(-1, n_input_features),
-            )
-
-            slots = slots.reshape(batch_size, -1, n_input_features)
-            slots = slots + self.mlp(self.norm_pre_ff(slots))
-
-        return slots
-
-
-class SlotAttention(nn.Module):
-    # pylint: disable=too-many-locals
+    # pylint: disable=too-many-locals,no-self-use
 
     def __init__(
         self,
@@ -88,6 +18,7 @@ class SlotAttention(nn.Module):
         iters=3,
         eps=1e-8,
         hidden_dim=128,
+        fixed_slots=False,
     ):
         super().__init__()
 
@@ -123,18 +54,47 @@ class SlotAttention(nn.Module):
         self.norm_slots = nn.LayerNorm(d_out)
         self.norm_pre_ff = nn.LayerNorm(d_out)
 
-    def forward(self, inputs):
-        x, _, batch = inputs
+        self.fixed_slots = fixed_slots
 
-        x = x.unsqueeze(1)
-        batch_size = torch.max(batch) + 1
-
+    def _get_slots(self, batch_size):
         n_s = self.num_slots
 
         mu = self.slots_mu.expand(batch_size, n_s, -1)
-        sigma = self.slots_logsigma.exp().expand(batch_size, n_s, -1)
+        slots = mu
 
-        slots = mu + sigma * torch.randn(mu.shape, device=x.device)
+        if not self.fixed_slots:
+            sigma = self.slots_logsigma.exp().expand(batch_size, n_s, -1)
+            slots += sigma * torch.randn(mu.shape)
+
+        return slots
+
+    def _slots_update(self, updates, slots_prev, batch_size):
+        slots = self.gru(
+            updates.reshape(batch_size, -1),
+            slots_prev.reshape(batch_size, -1),
+        )
+
+        slots = slots.reshape(batch_size, self.num_slots, -1)
+        return slots + self.mlp(self.norm_pre_ff(slots))
+
+    def _get_M(self, k, q):
+        return torch.matmul(k, q.transpose(2, 1)) * self.scale
+
+    def _get_attn(self, M):
+        return M.softmax(dim=-1) + self.eps
+
+    def _get_W(self, attn):
+        return attn / attn.sum(dim=1, keepdim=True)
+
+    def _get_updates(self, W, v):
+        return torch.matmul(W.transpose(2, 1), v)
+
+    def forward(self, inputs):
+        x = inputs
+        batch_size = x.shape[0]
+
+        slots = self._get_slots(batch_size)
+        slots = slots.to(x.device)
 
         x = self.norm_input(x)
         k = self.to_k(x)
@@ -145,19 +105,53 @@ class SlotAttention(nn.Module):
 
             slots = self.norm_slots(slots)
             q = self.to_q(slots)
+            M = self._get_M(k, q)
 
-            q_scatter = q[batch]
-            dots = torch.matmul(k, q_scatter.transpose(2, 1)) * self.scale
-            attn = dots.softmax(dim=1) + self.eps
-            W = attn / scatter(attn, batch, dim=0)[batch]
+            attn = self._get_attn(M)
+            W = self._get_W(attn)
+            updates = self._get_updates(W, v)
 
-            updates = scatter(W.transpose(2, 1) * v, batch, dim=0)
+            slots = self._slots_update(updates, slots_prev, batch_size)
 
-            slots = self.gru(
-                updates.reshape(batch_size, -1), slots_prev.reshape(batch_size, -1)
-            )
+        return slots
 
-            slots = slots.reshape(batch_size, self.num_slots, -1)
-            slots = slots + self.mlp(self.norm_pre_ff(slots))
+
+class SlotAttention(SlotAttentionRef):
+    # pylint: disable=arguments-differ
+
+    def _get_M(self, k, q, batch):
+        q_scatter = q[batch]
+        return torch.matmul(k, q_scatter.transpose(2, 1)) * self.scale
+
+    def _get_W(self, attn, batch):
+        return attn / scatter(attn, batch, dim=0)[batch]
+
+    def _get_updates(self, W, v, batch):
+        return scatter(W.transpose(2, 1) * v, batch, dim=0)
+
+    def forward(self, inputs):
+        x, _, batch = inputs
+        x = x.unsqueeze(1)
+        batch_size = torch.max(batch) + 1
+
+        slots = self._get_slots(batch_size)
+        slots = slots.to(x.device)
+
+        x = self.norm_input(x)
+        k = self.to_k(x)
+        v = self.to_v(x)
+
+        for _ in range(self.iters):
+            slots_prev = slots
+
+            slots = self.norm_slots(slots)
+            q = self.to_q(slots)
+            M = self._get_M(k, q, batch)
+
+            attn = self._get_attn(M)
+            W = self._get_W(attn, batch)
+            updates = self._get_updates(W, v, batch)
+
+            slots = self._slots_update(updates, slots_prev, batch_size)
 
         return slots
---
+++ tests/models/test_slot_attention.py
@@ -0,0 +1,202 @@
+# pylint: disable=protected-access,too-many-locals
+
+import pytest
+import torch
+from torch import nn
+
+from spg_experiments.models import slot_attention
+
+TEST_BATCH_SIZE = 16
+TEST_D0 = 10
+TEST_D1 = 20
+TEST_D2 = 15
+TEST_N = 5
+TEST_K = 7
+
+
+@pytest.fixture(name="even_batch")
+def fixture_even_batch():
+    x = torch.randn(TEST_BATCH_SIZE, TEST_N, TEST_D0)
+    return x
+
+
+@pytest.fixture(name="uneven_batch")
+def fixture_uneven_batch():
+    x = [torch.randn(N, TEST_D0) for N in torch.randint(1, 10, (TEST_BATCH_SIZE,))]
+    return x
+
+
+def compare_outputs(out_ref, out_new):
+    assert len(out_ref) == len(out_new)
+
+    for sample_ref, sample_new in zip(out_ref, out_new):
+        tensors_equal = torch.norm(sample_ref - sample_new).detach() == pytest.approx(
+            0, abs=1.0e-06
+        )
+        assert tensors_equal
+
+    return True
+
+
+def to_pyg(batch):
+    batch_idx = []
+    pyg_batch = []
+    for i, sample in enumerate(batch):
+        pyg_batch.append(sample)
+        batch_idx.append(torch.tensor([i] * len(sample)))
+
+    pyg_batch = torch.cat(pyg_batch)
+    batch_idx = torch.cat(batch_idx)
+    return pyg_batch, batch_idx
+
+
+def from_pyg(pyg_batch, batch_idx):
+    x = []
+    for i in range(torch.max(batch_idx) + 1):
+        x.append(pyg_batch[batch_idx == i])
+
+    return x
+
+
+def pyg_wrapper(fcn, x):
+    x, batch = to_pyg(x)
+    return from_pyg(fcn(x), batch)
+
+
+def layers_list():
+    layer_norm = nn.LayerNorm(TEST_D0)
+    lin_layer_1 = nn.Linear(TEST_D0, TEST_D1)
+    seq_layer = nn.Sequential(
+        nn.Linear(TEST_D0, TEST_D1),
+        nn.ReLU(),
+        nn.Linear(TEST_D1, TEST_D0),
+    )
+
+    torch.manual_seed(0)
+    sa_ref1 = slot_attention.SlotAttentionRef(TEST_K, TEST_D0, fixed_slots=True)
+    torch.manual_seed(0)
+    sa_ref2 = slot_attention.SlotAttentionRef(TEST_K, TEST_D0, fixed_slots=True)
+    torch.manual_seed(0)
+    sa_new1 = slot_attention.SlotAttention(TEST_K, TEST_D0, fixed_slots=True)
+    torch.manual_seed(0)
+    sa_new2 = slot_attention.SlotAttention(TEST_K, TEST_D0, fixed_slots=True)
+
+    def sa_wrapper(sa, x):
+        x, batch = to_pyg(x)
+        inputs = (x, None, batch)
+        return sa(inputs)
+
+    def sa_ref_get_M(sa, x):
+        batch_size = x.shape[0]
+        slots = sa._get_slots(batch_size)
+
+        k = sa.to_k(x)
+        q = sa.to_q(slots)
+        M = sa._get_M(k, q)
+        return M
+
+    def sa_ref_get_attn(sa, x):
+        M = sa_ref_get_M(sa, x)
+        attn = sa._get_attn(M)
+        return attn
+
+    def sa_ref_get_W(sa, x):
+        attn = sa_ref_get_attn(sa, x)
+        return sa._get_W(attn)
+
+    def sa_ref_get_updates(sa, x):
+        v = sa.to_v(x)
+        W = sa_ref_get_W(sa, x)
+        return sa._get_updates(W, v)
+
+    def _sa_new_get_M(sa, x):
+        x, batch = to_pyg(x)
+        x = x.unsqueeze(1)
+        batch_size = torch.max(batch) + 1
+
+        slots = sa._get_slots(batch_size)
+        k = sa.to_k(x)
+        q = sa.to_q(slots)
+        M = sa._get_M(k, q, batch)
+        return M, batch
+
+    def sa_new_get_M(sa, x):
+        M, batch = _sa_new_get_M(sa, x)
+        return from_pyg(M.squeeze(), batch)
+
+    def _sa_new_get_attn(sa, x):
+        M, batch = _sa_new_get_M(sa, x)
+        attn = sa._get_attn(M)
+        return attn, batch
+
+    def sa_new_get_attn(sa, x):
+        attn, batch = _sa_new_get_attn(sa, x)
+        return from_pyg(attn.squeeze(), batch)
+
+    def _sa_new_get_W(sa, x):
+        attn, batch = _sa_new_get_attn(sa, x)
+        W = sa._get_W(attn, batch)
+        return W, batch
+
+    def sa_new_get_W(sa, x):
+        W, batch = _sa_new_get_W(sa, x)
+        return from_pyg(W.squeeze(), batch)
+
+    def sa_new_get_updates(sa, inputs):
+        x, _ = to_pyg(inputs)
+        x = x.unsqueeze(1)
+        v = sa.to_v(x)
+        W, batch = _sa_new_get_W(sa, inputs)
+        updates = sa._get_updates(W, v, batch)
+        return updates
+
+    return [
+        [lambda x: x, lambda x: from_pyg(*to_pyg(x))],
+        [nn.Identity(), lambda x: pyg_wrapper(nn.Identity(), x)],
+        [layer_norm, lambda x: pyg_wrapper(layer_norm, x)],
+        [lin_layer_1, lambda x: pyg_wrapper(lin_layer_1, x)],
+        [seq_layer, lambda x: pyg_wrapper(seq_layer, x)],
+        [lambda x: sa_wrapper(sa_new1, x), lambda x: sa_wrapper(sa_new2, x)],
+        [sa_ref1, sa_ref2],
+        [sa_ref1.norm_input, lambda x: pyg_wrapper(sa_new1.norm_input, x)],
+        [sa_ref1.to_v, lambda x: pyg_wrapper(sa_new1.to_v, x)],
+        [sa_ref1.to_k, lambda x: pyg_wrapper(sa_new1.to_k, x)],
+        [sa_ref1.to_q, lambda x: pyg_wrapper(sa_new1.to_q, x)],
+        [
+            lambda x: sa_ref_get_M(sa_ref1, x),
+            lambda x: sa_new_get_M(sa_new1, x),
+        ],
+        [
+            lambda x: sa_ref_get_attn(sa_ref1, x),
+            lambda x: sa_new_get_attn(sa_new1, x),
+        ],
+        [
+            lambda x: sa_ref_get_W(sa_ref1, x),
+            lambda x: sa_new_get_W(sa_new1, x),
+        ],
+        [
+            lambda x: sa_ref_get_updates(sa_ref1, x),
+            lambda x: sa_new_get_updates(sa_new1, x),
+        ],
+        [sa_ref1, lambda x: sa_wrapper(sa_new1, x)],
+    ]
+
+
+@pytest.mark.parametrize("layers", layers_list())
+def test_even_batch(even_batch, layers):
+    layer_ref, layer_new = layers
+
+    out_ref = layer_ref(even_batch)
+    out_new = layer_new(even_batch)
+
+    assert compare_outputs(out_ref, out_new)
+
+
+# @pytest.mark.parametrize("layers", layers_list())
+# def test_uneven_batch(uneven_batch, layers):
+#     layer_0, layer_1 = layers
+
+#     out_0 = [layer_0(b) for b in uneven_batch]
+#     out_1 = layer_1(uneven_batch)
+
+#     assert compare_outputs(out_0, out_1)
