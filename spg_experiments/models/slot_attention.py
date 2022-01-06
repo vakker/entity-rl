193e65a088159914f20334dc72ee8eff3a8782a7
Vince Jankovics <vincejankovics@gmail.com>
2022-01-06 03:50:03 +0000

    Slot attention PyG refactor

--- spg_experiments/models/slot_attention.py
+++ spg_experiments/models/slot_attention.py
@@ -3,9 +3,10 @@
 import torch
 from torch import nn
 from torch.nn import init
+from torch_scatter import scatter
 
 
-class SlotAttention(nn.Module):
+class SlotAttentionRef(nn.Module):
     # pylint: disable=too-many-locals
 
     def __init__(self, num_slots, dim, iters=3, eps=1e-8, hidden_dim=128):
@@ -70,6 +71,93 @@ class SlotAttention(nn.Module):
             )
 
             slots = slots.reshape(batch_size, -1, n_input_features)
+            slots = slots + self.mlp(self.norm_pre_ff(slots))
+
+        return slots
+
+
+class SlotAttention(nn.Module):
+    # pylint: disable=too-many-locals
+
+    def __init__(
+        self,
+        num_slots,
+        dim,
+        d_in=None,
+        d_out=None,
+        iters=3,
+        eps=1e-8,
+        hidden_dim=128,
+    ):
+        super().__init__()
+
+        if d_in is None:
+            d_in = dim
+
+        if d_out is None:
+            d_out = dim
+
+        self.num_slots = num_slots
+        self.iters = iters
+        self.eps = eps
+        self.scale = dim ** -0.5
+
+        self.slots_mu = nn.Parameter(torch.randn(1, 1, d_out))
+
+        self.slots_logsigma = nn.Parameter(torch.zeros(1, 1, d_out))
+        init.xavier_uniform_(self.slots_logsigma)
+
+        self.to_q = nn.Linear(d_out, dim)
+        self.to_k = nn.Linear(d_in, dim)
+        self.to_v = nn.Linear(d_in, d_out)
+
+        self.gru = nn.GRUCell(num_slots * d_out, num_slots * d_out)
+
+        self.mlp = nn.Sequential(
+            nn.Linear(d_out, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, d_out),
+        )
+
+        self.norm_input = nn.LayerNorm(d_in)
+        self.norm_slots = nn.LayerNorm(d_out)
+        self.norm_pre_ff = nn.LayerNorm(d_out)
+
+    def forward(self, inputs):
+        x, _, batch = inputs
+
+        x = x.unsqueeze(1)
+        batch_size = torch.max(batch) + 1
+
+        n_s = self.num_slots
+
+        mu = self.slots_mu.expand(batch_size, n_s, -1)
+        sigma = self.slots_logsigma.exp().expand(batch_size, n_s, -1)
+
+        slots = mu + sigma * torch.randn(mu.shape, device=x.device)
+
+        x = self.norm_input(x)
+        k = self.to_k(x)
+        v = self.to_v(x)
+
+        for _ in range(self.iters):
+            slots_prev = slots
+
+            slots = self.norm_slots(slots)
+            q = self.to_q(slots)
+
+            q_scatter = q[batch]
+            dots = torch.matmul(k, q_scatter.transpose(2, 1)) * self.scale
+            attn = dots.softmax(dim=1) + self.eps
+            W = attn / scatter(attn, batch, dim=0)[batch]
+
+            updates = scatter(W.transpose(2, 1) * v, batch, dim=0)
+
+            slots = self.gru(
+                updates.reshape(batch_size, -1), slots_prev.reshape(batch_size, -1)
+            )
+
+            slots = slots.reshape(batch_size, self.num_slots, -1)
             slots = slots + self.mlp(self.norm_pre_ff(slots))
 
         return slots
