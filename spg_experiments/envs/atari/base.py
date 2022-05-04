10ce0dede926454adeab0f4632a107e0d13d3c94
Vince Jankovics <vincejankovics@gmail.com>
2022-05-04 20:48:10 +0100

    Atari wrapper fix

--- spg_experiments/envs/atari/base.py
+++ spg_experiments/envs/atari/base.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 import gym
 import numpy as np
 from ray.rllib.env.wrappers import atari_wrappers as wrappers
+from skimage import transform
 
 
 class AtariEnv(gym.Env, ABC):
@@ -21,13 +22,16 @@ class AtariEnv(gym.Env, ABC):
         if config.get("wrap", True):
             self._env = wrap_deepmind(self._env)
 
+        self._env = ResizeEnv(self._env, (128, 128))
+
         self.video_dir = config.get("video_dir")
 
         self.episodes = 0
 
         self.time_steps = 0
         self.obs_raw = None
-        self._crop = [25, 10]
+        self._crop = [0, 0]
+        # self._crop = [25, 10]
 
         self.action_space = self._env.action_space
         self._obs_space = None
@@ -39,13 +43,13 @@ class AtariEnv(gym.Env, ABC):
 
         return self._obs_space
 
-    @abstractmethod
     def _set_obs_space(self):
-        pass
+        orig = self._env.observation_space
+        cropped_shape = (orig.shape[0] - sum(self._crop), orig.shape[1], orig.shape[2])
+        return gym.spaces.Box(0, 1, cropped_shape, dtype=np.float32)
 
     def step(self, action):
         obs, reward, done, info = self._env.step(action)
-
         return self._process_obs(obs), reward, done, info
 
     def reset(self):
@@ -64,7 +68,10 @@ class AtariEnv(gym.Env, ABC):
         self._env.close()
 
     def crop_obs(self, obs):
-        return obs[self._crop[0] : -self._crop[1]]
+        if self._crop[1] > 0:
+            return obs[self._crop[0] : -self._crop[1]]
+
+        return obs[self._crop[0] :]
 
     def _process_obs(self, obs):
         return self.process_obs(self.crop_obs(obs))
@@ -79,11 +86,6 @@ class AtariEnv(gym.Env, ABC):
 
 
 class AtariRaw(AtariEnv):
-    def _set_obs_space(self):
-        orig = self._env.observation_space
-        cropped_shape = (orig.shape[0] - sum(self._crop), orig.shape[1], orig.shape[2])
-        return gym.spaces.Box(0, 1, cropped_shape, dtype=np.float32)
-
     def process_obs(self, obs):
         self.obs_raw = obs
         return obs / 255
@@ -92,10 +94,26 @@ class AtariRaw(AtariEnv):
         return self.obs_raw
 
 
+class ResizeEnv(gym.ObservationWrapper):
+    def __init__(self, env, size):
+        super().__init__(env)
+        self._size = size
+
+        self.observation_space = gym.spaces.Box(
+            low=np.amin(env.observation_space.low),
+            high=np.amax(env.observation_space.high),
+            shape=(size[0], size[1], env.observation_space.shape[-1]),
+            dtype=env.observation_space.dtype,
+        )
+
+    def observation(self, observation):
+        return transform.resize(observation, self._size)
+
+
 class SkipEnv(gym.Wrapper):
     def __init__(self, env, skip=4):
         """Return only every `skip`-th frame"""
-        gym.Wrapper.__init__(self, env)
+        super().__init__(env)
         self._skip = skip
 
     def step(self, action):
@@ -122,7 +140,7 @@ def wrap_deepmind(env, framestack=True, skip=4, stack=4):
     if "FIRE" in env.unwrapped.get_action_meanings():
         env = wrappers.FireResetEnv(env)
     # env = wrappers.WarpFrame(env, dim) # don't warp
-    # env = ScaledFloatFrame(env)  # TODO: use for dqn?
+    # env = wrappers.ScaledFloatFrame(env)  # TODO: use for dqn?
     # env = ClipRewardEnv(env)  # reward clipping is handled by policy eval
     # 4x image framestacking.
     if framestack is True:
