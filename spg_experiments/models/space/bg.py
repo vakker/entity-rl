d59b44d010dc1795374a151f5af29bab9563b48d
Vince Jankovics <vincejankovics@gmail.com>
2022-05-04 21:12:44 +0100

    Formatting

--- spg_experiments/models/space/bg.py
+++ spg_experiments/models/space/bg.py
@@ -1,24 +1,25 @@
 import torch
 from torch import nn
-from attrdict import AttrDict
-from torch.nn import functional as F
-from torch.distributions.normal import Normal
 from torch.distributions.kl import kl_divergence
+from torch.distributions.normal import Normal
+from torch.nn import functional as F
+
 from .arch import arch
 
 
 class SpaceBg(nn.Module):
-    
     def __init__(self):
         nn.Module.__init__(self)
-        
+
         self.image_enc = ImageEncoderBg()
-        
+
         # Compute mask hidden states given image features
-        self.rnn_mask = nn.LSTMCell(arch.z_mask_dim + arch.img_enc_dim_bg, arch.rnn_mask_hidden_dim)
+        self.rnn_mask = nn.LSTMCell(
+            arch.z_mask_dim + arch.img_enc_dim_bg, arch.rnn_mask_hidden_dim
+        )
         self.rnn_mask_h = nn.Parameter(torch.zeros(arch.rnn_mask_hidden_dim))
         self.rnn_mask_c = nn.Parameter(torch.zeros(arch.rnn_mask_hidden_dim))
-        
+
         # Dummy z_mask for first step of rnn_mask
         self.z_mask_0 = nn.Parameter(torch.zeros(arch.z_mask_dim))
         # Predict mask latent given h
@@ -34,10 +35,16 @@ class SpaceBg(nn.Module):
             self.comp_decoder = CompDecoderStrong()
 
         # ==== Prior related ====
-        self.rnn_mask_prior = nn.LSTMCell(arch.z_mask_dim, arch.rnn_mask_prior_hidden_dim)
+        self.rnn_mask_prior = nn.LSTMCell(
+            arch.z_mask_dim, arch.rnn_mask_prior_hidden_dim
+        )
         # Initial h and c
-        self.rnn_mask_h_prior = nn.Parameter(torch.zeros(arch.rnn_mask_prior_hidden_dim))
-        self.rnn_mask_c_prior = nn.Parameter(torch.zeros(arch.rnn_mask_prior_hidden_dim))
+        self.rnn_mask_h_prior = nn.Parameter(
+            torch.zeros(arch.rnn_mask_prior_hidden_dim)
+        )
+        self.rnn_mask_c_prior = nn.Parameter(
+            torch.zeros(arch.rnn_mask_prior_hidden_dim)
+        )
         # Compute mask latents
         self.predict_mask_prior = PredictMask()
         # Compute component latents
@@ -45,14 +52,15 @@ class SpaceBg(nn.Module):
         # ==== Prior related ====
 
         self.bg_sigma = arch.bg_sigma
-    
+
     def anneal(self, global_step):
         pass
-    
+
+    # pylint: disable=unused-argument,too-many-statements
     def forward(self, x, global_step):
         """
         Background inference backward pass
-        
+
         :param x: shape (B, C, H, W)
         :param global_step: global training step
         :return:
@@ -62,27 +70,27 @@ class SpaceBg(nn.Module):
             log: a dictionary containing things for visualization
         """
         B = x.size(0)
-        
+
         # (B, D)
         x_enc = self.image_enc(x)
-        
+
         # Mask and component latents over the K slots
         masks = []
         z_masks = []
         # These two are Normal instances
         z_mask_posteriors = []
         z_comp_posteriors = []
-        
+
         # Initialization: encode x and dummy z_mask_0
         z_mask = self.z_mask_0.expand(B, arch.z_mask_dim)
         h = self.rnn_mask_h.expand(B, arch.rnn_mask_hidden_dim)
         c = self.rnn_mask_c.expand(B, arch.rnn_mask_hidden_dim)
-        
+
         for i in range(arch.K):
             # Encode x and z_{mask, 1:k}, (b, D)
             rnn_input = torch.cat((z_mask, x_enc), dim=1)
             (h, c) = self.rnn_mask(rnn_input, (h, c))
-            
+
             # Predict next mask from x and z_{mask, 1:k-1}
             z_mask_loc, z_mask_scale = self.predict_mask(h)
             z_mask_post = Normal(z_mask_loc, z_mask_scale)
@@ -92,64 +100,72 @@ class SpaceBg(nn.Module):
             # Decode masks
             mask = self.mask_decoder(z_mask)
             masks.append(mask)
-        
+
         # (B, K, 1, H, W), in range (0, 1)
         masks = torch.stack(masks, dim=1)
-        
+
         # SBP to ensure they sum to 1
         masks = self.SBP(masks)
         # An alternative is to use softmax
         # masks = F.softmax(masks, dim=1)
-        
+
         B, K, _, H, W = masks.size()
-        
+
         # Reshape (B, K, 1, H, W) -> (B*K, 1, H, W)
         masks = masks.view(B * K, 1, H, W)
-        
+
         # Concatenate images (B*K, 4, H, W)
-        comp_vae_input = torch.cat(((masks + 1e-5).log(), x[:, None].repeat(1, K, 1, 1, 1).view(B * K, 3, H, W)), dim=1)
-        
+        comp_vae_input = torch.cat(
+            (
+                (masks + 1e-5).log(),
+                x[:, None].repeat(1, K, 1, 1, 1).view(B * K, 3, H, W),
+            ),
+            dim=1,
+        )
+
         # Component latents, each (B*K, L)
         z_comp_loc, z_comp_scale = self.comp_encoder(comp_vae_input)
         z_comp_post = Normal(z_comp_loc, z_comp_scale)
         z_comp = z_comp_post.rsample()
-        
+
         # Record component posteriors here. We will use this for computing KL
         z_comp_loc_reshape = z_comp_loc.view(B, K, -1)
         z_comp_scale_reshape = z_comp_scale.view(B, K, -1)
         for i in range(arch.K):
-            z_comp_post_this = Normal(z_comp_loc_reshape[:, i], z_comp_scale_reshape[:, i])
+            z_comp_post_this = Normal(
+                z_comp_loc_reshape[:, i], z_comp_scale_reshape[:, i]
+            )
             z_comp_posteriors.append(z_comp_post_this)
-        
+
         # Decode into component images, (B*K, 3, H, W)
         comps = self.comp_decoder(z_comp)
-        
+
         # Reshape (B*K, ...) -> (B, K, 3, H, W)
         comps = comps.view(B, K, 3, H, W)
         masks = masks.view(B, K, 1, H, W)
-        
+
         # Now we are ready to compute the background likelihoods
         # (B, K, 3, H, W)
         comp_dist = Normal(comps, torch.full_like(comps, self.bg_sigma))
         log_likelihoods = comp_dist.log_prob(x[:, None].expand_as(comps))
-        
+
         # (B, K, 3, H, W) -> (B, 3, H, W), mixture likelihood
         log_sum = log_likelihoods + (masks + 1e-5).log()
         bg_likelihood = torch.logsumexp(log_sum, dim=1)
 
         # Background reconstruction
         bg = (comps * masks).sum(dim=1)
-        
+
         # Below we compute priors and kls
-        
+
         # Conditional KLs
         z_mask_total_kl = 0.0
         z_comp_total_kl = 0.0
-        
+
         # Initial h and c. This is h_1 and c_1 in the paper
         h = self.rnn_mask_h_prior.expand(B, arch.rnn_mask_prior_hidden_dim)
         c = self.rnn_mask_c_prior.expand(B, arch.rnn_mask_prior_hidden_dim)
-        
+
         for i in range(arch.K):
             # Compute prior distribution over z_masks
             z_mask_loc_prior, z_mask_scale_prior = self.predict_mask_prior(h)
@@ -163,23 +179,23 @@ class SpaceBg(nn.Module):
             # (B,)
             z_mask_total_kl += z_mask_kl
             z_comp_total_kl += z_comp_kl
-            
+
             # Compute next state. Note we condition we posterior samples.
             # Again, this is conditional prior.
             (h, c) = self.rnn_mask_prior(z_masks[i], (h, c))
-            
+
         # For visualization
         kl_bg = z_mask_total_kl + z_comp_total_kl
         log = {
             # (B, K, 3, H, W)
-            'comps': comps,
+            "comps": comps,
             # (B, 1, 3, H, W)
-            'masks': masks,
+            "masks": masks,
             # (B, 3, H, W)
-            'bg': bg,
-            'kl_bg': kl_bg
+            "bg": bg,
+            "kl_bg": kl_bg,
         }
-        
+
         return bg_likelihood, bg, kl_bg, log
 
     @staticmethod
@@ -189,8 +205,8 @@ class SpaceBg(nn.Module):
         :param: masks (B, K, 1, H, W). In range (0, 1)
         :return: (B, K, 1, H, W)
         """
-        B, K, _, H, W = masks.size()
-    
+        K = masks.shape[1]
+
         # (B, 1, H, W)
         remained = torch.ones_like(masks[:, 0])
         # remained = torch.ones_like(masks[:, 0]) - fg_mask
@@ -202,13 +218,13 @@ class SpaceBg(nn.Module):
                 mask = remained
             remained = remained - mask
             new_masks.append(mask)
-    
+
         new_masks = torch.stack(new_masks, dim=1)
-    
+
         return new_masks
-            
 
 
+# pylint: disable=no-self-use
 class Flatten(nn.Module):
     def forward(self, x):
         return x.reshape(x.shape[0], -1)
@@ -216,7 +232,7 @@ class Flatten(nn.Module):
 
 class ImageEncoderBg(nn.Module):
     """Background image encoder"""
-    
+
     def __init__(self):
         embed_size = arch.img_shape[0] // 16
         nn.Module.__init__(self)
@@ -238,7 +254,7 @@ class ImageEncoderBg(nn.Module):
             nn.Linear(64 * embed_size ** 2, arch.img_enc_dim_bg),
             nn.ELU(),
         )
-    
+
     def forward(self, x):
         """
         Encoder image into a feature vector
@@ -254,39 +270,38 @@ class PredictMask(nn.Module):
     """
     Predict z_mask given states from rnn. Used in inference
     """
-    
+
     def __init__(self):
         nn.Module.__init__(self)
         self.fc = nn.Linear(arch.rnn_mask_hidden_dim, arch.z_mask_dim * 2)
-    
+
     def forward(self, h):
         """
         Predict z_mask given states from rnn. Used in inference
-    
+
         :param h: hidden state from rnn_mask
         :return:
             z_mask_loc: (B, D)
             z_mask_scale: (B, D)
-        
+
         """
         x = self.fc(h)
-        z_mask_loc = x[:, :arch.z_mask_dim]
-        z_mask_scale = F.softplus(x[:, arch.z_mask_dim:]) + 1e-4
-        
+        z_mask_loc = x[:, : arch.z_mask_dim]
+        z_mask_scale = F.softplus(x[:, arch.z_mask_dim :]) + 1e-4
+
         return z_mask_loc, z_mask_scale
 
 
 class MaskDecoder(nn.Module):
     """Decode z_mask into mask"""
-    
+
     def __init__(self):
-        super(MaskDecoder, self).__init__()
-        
+        super().__init__()
+
         self.dec = nn.Sequential(
             nn.Conv2d(arch.z_mask_dim, 256, 1),
             nn.CELU(),
             nn.GroupNorm(16, 256),
-            
             nn.Conv2d(256, 256 * 4 * 4, 1),
             nn.PixelShuffle(4),
             nn.CELU(),
@@ -294,7 +309,6 @@ class MaskDecoder(nn.Module):
             nn.Conv2d(256, 256, 3, 1, 1),
             nn.CELU(),
             nn.GroupNorm(16, 256),
-            
             nn.Conv2d(256, 128 * 2 * 2, 1),
             nn.PixelShuffle(2),
             nn.CELU(),
@@ -302,7 +316,6 @@ class MaskDecoder(nn.Module):
             nn.Conv2d(128, 128, 3, 1, 1),
             nn.CELU(),
             nn.GroupNorm(16, 128),
-            
             nn.Conv2d(128, 64 * 4 * 4, 1),
             nn.PixelShuffle(4),
             nn.CELU(),
@@ -310,7 +323,6 @@ class MaskDecoder(nn.Module):
             nn.Conv2d(64, 64, 3, 1, 1),
             nn.CELU(),
             nn.GroupNorm(8, 64),
-            
             nn.Conv2d(64, 16 * 4 * 4, 1),
             nn.PixelShuffle(4),
             nn.CELU(),
@@ -318,18 +330,16 @@ class MaskDecoder(nn.Module):
             nn.Conv2d(16, 16, 3, 1, 1),
             nn.CELU(),
             nn.GroupNorm(4, 16),
-            
             nn.Conv2d(16, 16, 3, 1, 1),
             nn.CELU(),
             nn.GroupNorm(4, 16),
-            nn.Conv2d(16, 1, 3, 1, 1)
-        
+            nn.Conv2d(16, 1, 3, 1, 1),
         )
-    
+
     def forward(self, z_mask):
         """
         Decode z_mask into mask
-        
+
         :param z_mask: (B, D)
         :return: mask: (B, 1, H, W)
         """
@@ -344,10 +354,10 @@ class CompEncoder(nn.Module):
     """
     Predict component latent parameters given image and predicted mask concatenated
     """
-    
+
     def __init__(self):
         nn.Module.__init__(self)
-        
+
         embed_size = arch.img_shape[0] // 16
         self.enc = nn.Sequential(
             nn.Conv2d(4, 32, 3, 2, 1),
@@ -366,19 +376,19 @@ class CompEncoder(nn.Module):
             # 16x downsampled: (64, 4, 4)
             nn.Linear(64 * embed_size ** 2, arch.z_comp_dim * 2),
         )
-    
+
     def forward(self, x):
         """
         Predict component latent parameters given image and predicted mask concatenated
-        
+
         :param x: (B, 3+1, H, W). Image and mask concatenated
         :return:
             z_comp_loc: (B, D)
             z_comp_scale: (B, D)
         """
         x = self.enc(x)
-        z_comp_loc = x[:, :arch.z_comp_dim]
-        z_comp_scale = F.softplus(x[:, arch.z_comp_dim:]) + 1e-4
+        z_comp_loc = x[:, : arch.z_comp_dim]
+        z_comp_scale = F.softplus(x[:, arch.z_comp_dim :]) + 1e-4
 
         return z_comp_loc, z_comp_scale
 
@@ -387,14 +397,11 @@ class SpatialBroadcast(nn.Module):
     """
     Broadcast a 1-D variable to 3-D, plus two coordinate dimensions
     """
-    
-    def __init__(self):
-        nn.Module.__init__(self)
-    
+
     def forward(self, x, width, height):
         """
         Broadcast a 1-D variable to 3-D, plus two coordinate dimensions
-        
+
         :param x: (B, L)
         :param width: W
         :param height: H
@@ -412,10 +419,10 @@ class SpatialBroadcast(nn.Module):
         coords = torch.stack((xx, yy), dim=0)
         # (B, 2, H, W)
         coords = coords[None].expand(B, 2, height, width)
-        
+
         # (B, L + 2, W, H)
         x = torch.cat((x, coords), dim=1)
-        
+
         return x
 
 
@@ -423,7 +430,7 @@ class CompDecoder(nn.Module):
     """
     Decoder z_comp into component image
     """
-    
+
     def __init__(self):
         nn.Module.__init__(self)
         self.spatial_broadcast = SpatialBroadcast()
@@ -444,7 +451,7 @@ class CompDecoder(nn.Module):
             # 16x downsampled: (32, 4, 4)
             nn.Conv2d(32, 3, 1, 1),
         )
-    
+
     def forward(self, z_comp):
         """
         :param z_comp: (B, L)
@@ -460,15 +467,13 @@ class CompDecoder(nn.Module):
 
 
 class CompDecoderStrong(nn.Module):
-    
     def __init__(self):
-        super(CompDecoderStrong, self).__init__()
-        
+        super().__init__()
+
         self.dec = nn.Sequential(
             nn.Conv2d(arch.z_comp_dim, 256, 1),
             nn.CELU(),
             nn.GroupNorm(16, 256),
-            
             nn.Conv2d(256, 256 * 4 * 4, 1),
             nn.PixelShuffle(4),
             nn.CELU(),
@@ -476,7 +481,6 @@ class CompDecoderStrong(nn.Module):
             nn.Conv2d(256, 256, 3, 1, 1),
             nn.CELU(),
             nn.GroupNorm(16, 256),
-            
             nn.Conv2d(256, 128 * 4 * 4, 1),
             nn.PixelShuffle(4),
             nn.CELU(),
@@ -484,7 +488,6 @@ class CompDecoderStrong(nn.Module):
             nn.Conv2d(128, 128, 3, 1, 1),
             nn.CELU(),
             nn.GroupNorm(16, 128),
-            
             nn.Conv2d(128, 64 * 2 * 2, 1),
             nn.PixelShuffle(2),
             nn.CELU(),
@@ -492,7 +495,6 @@ class CompDecoderStrong(nn.Module):
             nn.Conv2d(64, 64, 3, 1, 1),
             nn.CELU(),
             nn.GroupNorm(8, 64),
-            
             nn.Conv2d(64, 16 * 4 * 4, 1),
             nn.PixelShuffle(4),
             nn.CELU(),
@@ -500,14 +502,12 @@ class CompDecoderStrong(nn.Module):
             nn.Conv2d(16, 16, 3, 1, 1),
             nn.CELU(),
             nn.GroupNorm(4, 16),
-            
             nn.Conv2d(16, 16, 3, 1, 1),
             nn.CELU(),
             nn.GroupNorm(4, 16),
-            nn.Conv2d(16, 3, 3, 1, 1)
-        
+            nn.Conv2d(16, 3, 3, 1, 1),
         )
-    
+
     def forward(self, x):
         """
 
@@ -523,7 +523,7 @@ class PredictComp(nn.Module):
     """
     Predict component latents given mask latent
     """
-    
+
     def __init__(self):
         nn.Module.__init__(self)
         self.mlp = nn.Sequential(
@@ -533,7 +533,7 @@ class PredictComp(nn.Module):
             nn.ELU(),
             nn.Linear(arch.predict_comp_hidden_dim, arch.z_comp_dim * 2),
         )
-    
+
     def forward(self, h):
         """
         :param h: (B, D) hidden state from rnn_mask
@@ -542,7 +542,7 @@ class PredictComp(nn.Module):
             z_comp_scale: (B, D)
         """
         x = self.mlp(h)
-        z_comp_loc = x[:, :arch.z_comp_dim]
-        z_comp_scale = F.softplus(x[:, arch.z_comp_dim:]) + 1e-4
-        
+        z_comp_loc = x[:, : arch.z_comp_dim]
+        z_comp_scale = F.softplus(x[:, arch.z_comp_dim :]) + 1e-4
+
         return z_comp_loc, z_comp_scale
--- spg_experiments/models/space/utils.py
+++ spg_experiments/models/space/utils.py
@@ -5,14 +5,20 @@ from torch.distributions.utils import broadcast_all
 
 
 def spatial_transform(image, z_where, out_dims, inverse=False):
-    """ spatial transformer network used to scale and shift input according to z_where in:
-            1/ x -> x_att   -- shapes (H, W) -> (attn_window, attn_window) -- thus inverse = False
-            2/ y_att -> y   -- (attn_window, attn_window) -> (H, W) -- thus inverse = True
+    """spatial transformer network used to scale and
+       shift input according to z_where in:
+            1/ x -> x_att   -- shapes (H, W) -> (attn_window, attn_window)
+                            -- thus inverse = False
+            2/ y_att -> y   -- (attn_window, attn_window) -> (H, W)
+                            -- thus inverse = True
     inverting the affine transform as follows: A_inv ( A * image ) = image
-    A = [R | T] where R is rotation component of angle alpha, T is [tx, ty] translation component
+    A = [R | T] where R is rotation component of angle alpha,
+    T is [tx, ty] translation component
     A_inv rotates by -alpha and translates by [-tx, -ty]
     if x' = R * x + T  -->  x = R_inv * (x' - T) = R_inv * x - R_inv * T
-    here, z_where is 3-dim [scale, tx, ty] so inverse transform is [1/scale, -tx/scale, -ty/scale]
+    here, z_where is 3-dim [scale, tx, ty]
+    so inverse transform is [1/scale, -tx/scale, -ty/scale]
+
     R = [[s, 0],  ->  R_inv = [[1/s, 0],
          [0, s]]               [0, 1/s]]
     """
@@ -23,17 +29,22 @@ def spatial_transform(image, z_where, out_dims, inverse=False):
     theta[:, 1, 1] = z_where[:, 1] if not inverse else 1 / (z_where[:, 1] + 1e-9)
 
     # set translation
-    theta[:, 0, -1] = z_where[:, 2] if not inverse else - z_where[:, 2] / (z_where[:, 0] + 1e-9)
-    theta[:, 1, -1] = z_where[:, 3] if not inverse else - z_where[:, 3] / (z_where[:, 1] + 1e-9)
+    theta[:, 0, -1] = (
+        z_where[:, 2] if not inverse else -z_where[:, 2] / (z_where[:, 0] + 1e-9)
+    )
+    theta[:, 1, -1] = (
+        z_where[:, 3] if not inverse else -z_where[:, 3] / (z_where[:, 1] + 1e-9)
+    )
     # 2. construct sampling grid
-    grid = F.affine_grid(theta, torch.Size(out_dims), align_corners = False)
+    grid = F.affine_grid(theta, torch.Size(out_dims), align_corners=False)
     # 3. sample image from grid
-    return F.grid_sample(image, grid, align_corners = False)
+    return F.grid_sample(image, grid, align_corners=False)
+
 
 def linear_annealing(device, step, start_step, end_step, start_value, end_value):
     """
     Linear annealing
-    
+
     :param x: original value. Only for getting device
     :param step: current global step
     :param start_step: when to start changing value
@@ -49,26 +60,27 @@ def linear_annealing(device, step, start_step, end_step, start_value, end_value)
         x = torch.tensor(start_value + slope * (step - start_step), device=device)
     else:
         x = torch.tensor(end_value, device=device)
-    
+
     return x
 
 
+# pylint: disable=abstract-method
 class NumericalRelaxedBernoulli(RelaxedBernoulli):
     """
     This is a bit weird. In essence it is just RelaxedBernoulli with logit as input.
     """
-    
+
     def rsample(self, sample_shape=torch.Size()):
         return self.base_dist.rsample(sample_shape)
-    
+
     def log_prob(self, value):
         if self._validate_args:
             self._validate_sample(value)
         logits, value = broadcast_all(self.logits, value)
         diff = logits - value.mul(self.temperature)
-        
+
         out = self.temperature.log() + diff - 2 * diff.exp().log1p()
-        
+
         return out
 
 
@@ -80,9 +92,12 @@ def kl_divergence_bern_bern(z_pres_logits, prior_pres_prob, eps=1e-15):
     :return: kl divergence, (B, ...)
     """
     z_pres_probs = torch.sigmoid(z_pres_logits)
-    kl = z_pres_probs * (torch.log(z_pres_probs + eps) - torch.log(prior_pres_prob + eps)) + \
-         (1 - z_pres_probs) * (torch.log(1 - z_pres_probs + eps) - torch.log(1 - prior_pres_prob + eps))
-    
+    kl = z_pres_probs * (
+        torch.log(z_pres_probs + eps) - torch.log(prior_pres_prob + eps)
+    ) + (1 - z_pres_probs) * (
+        torch.log(1 - z_pres_probs + eps) - torch.log(1 - prior_pres_prob + eps)
+    )
+
     return kl
 
 
@@ -95,12 +110,15 @@ def get_boundary_kernel_new(kernel_size=32, boundary_width=6):
     ============
     size will be (kernel_size, kernel_size)
     """
-    filter = torch.zeros(kernel_size, kernel_size)
-    filter[:, :] = 1.0 / (kernel_size ** 2)
+    kernel = torch.zeros(kernel_size, kernel_size)
+    kernel[:, :] = 1.0 / (kernel_size ** 2)
     # Set center to zero
-    filter[boundary_width:kernel_size - boundary_width, boundary_width:kernel_size - boundary_width] = 0.0
-    
-    return filter
+    kernel[
+        boundary_width : kernel_size - boundary_width,
+        boundary_width : kernel_size - boundary_width,
+    ] = 0.0
+
+    return kernel
 
 
 def get_boundary_kernel(kernel_size=32, sigma=20, channels=1, beta=1.0):
@@ -110,7 +128,7 @@ def get_boundary_kernel(kernel_size=32, sigma=20, channels=1, beta=1.0):
     # Create a x, y coordinate grid of shape (kernel_size, kernel_size, 2)
     x_coord = torch.arange(kernel_size)
     boundary_kernel = x_coord.repeat(kernel_size).view(kernel_size, kernel_size).float()
-    
+
     part_sum = 1.0
     # boundary = int((1 - math.sqrt(2) / 2) * kernel_size / 2)
     boundary = int((kernel_size - sigma) / 2)
@@ -118,16 +136,21 @@ def get_boundary_kernel(kernel_size=32, sigma=20, channels=1, beta=1.0):
     num_boundary = kernel_size * kernel_size - num_center
     boundary_kernel.data[:, :] = -part_sum / (num_boundary + num_center) * beta
     boundary_kernel.data[
-    boundary: kernel_size - boundary, boundary: kernel_size - boundary
+        boundary : kernel_size - boundary, boundary : kernel_size - boundary
     ] = part_sum / (num_boundary + num_center)
-    
+
     # Reshape to 2d depthwise convolutional weight
     boundary_kernel = boundary_kernel.view(1, 1, kernel_size, kernel_size)
     boundary_kernel = boundary_kernel.repeat(channels, 1, 1, 1)
-    
-    boundary_filter = torch.nn.Conv2d(in_channels=channels, out_channels=channels,
-                                      kernel_size=kernel_size, groups=channels, bias=False)
-    
+
+    boundary_filter = torch.nn.Conv2d(
+        in_channels=channels,
+        out_channels=channels,
+        kernel_size=kernel_size,
+        groups=channels,
+        bias=False,
+    )
+
     boundary_filter.weight.data = boundary_kernel
     boundary_filter.weight.requires_grad = False
     return boundary_filter
