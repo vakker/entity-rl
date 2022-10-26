eaf0f3bc6f90b5b40e2cd890712b9b61d4009a7f
Vince Jankovics <vincejankovics@gmail.com>
2022-10-26 20:30:35 +0100

    Train args fix

--- scripts/train.py
+++ scripts/train.py
@@ -15,10 +15,6 @@ torch.backends.cudnn.benchmark = True
 def main(args):
     args_dict = vars(args)
 
-    assert (args.max_iters is None) ^ (
-        args.max_steps is None
-    ), "Provide either max iters or steps."
-
     ray.init(
         address=args_dict["head_ip"],
         _node_ip_address=args_dict["node_ip"],
@@ -64,15 +60,15 @@ if __name__ == "__main__":
     PARSER.add_argument("--tune", action="store_true")
     PARSER.add_argument("--no-sched", action="store_true")
     PARSER.add_argument("--logdir")
-    PARSER.add_argument("--max-iters", type=int)
-    PARSER.add_argument("--max-steps", type=int)
+    PARSER.add_argument("--stop-attr", type=str, default="timesteps_total")
+    PARSER.add_argument("--stop-at", type=int)
     PARSER.add_argument("--num-samples", type=int, default=1)
     PARSER.add_argument("--local", action="store_true")
     PARSER.add_argument("--concurrency", type=int, default=1)
     PARSER.add_argument("--num-workers", type=int, default=1)
     PARSER.add_argument("--cpus-per-worker", type=float, default=1.0)
     PARSER.add_argument("--envs-per-worker", type=int, default=1)
-    PARSER.add_argument("--grace-period", type=int, default=50)
+    PARSER.add_argument("--grace-period", type=float, default=0.25)
     PARSER.add_argument("--num-gpus", type=float, default=1.0)
     PARSER.add_argument("--checkpoint-freq", type=int, default=1)
     PARSER.add_argument("--keep-all-chkp", action="store_true")
