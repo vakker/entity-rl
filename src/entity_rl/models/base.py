Combined normalization added, utils fixed

src/entity_rl/models/base.py
@@ -10,8 +10,12 @@ from ray.rllib.utils.typing import ModelConfigDict, TensorType
 from torch import nn
 
 
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+def get_num_params(module):
+    num_params = {
+        "all": sum(p.numel() for p in module.parameters()),
+        "trainable": sum(p.numel() for p in module.parameters() if p.requires_grad),
+    }
+    return num_params
 
 
 class BaseModule(nn.Module, ABC):
@@ -21,10 +25,7 @@ class BaseModule(nn.Module, ABC):
 
     @property
     def num_params(self):
-        s = 0
-        for p in self.parameters():
-            s += p.sum()
-        return s.item()
+        return get_num_params(self)
 
 
 class BasePolicy(TorchModelV2, BaseModule, ABC):

src/entity_rl/models/combined.py
@@ -1,14 +1,10 @@
 import sys
-from typing import List
 
 import torch
-
-# from ray.rllib.core.models.base import ENCODER_OUT, Encoder
-# from ray.rllib.core.models.configs import ModelConfig
-# from ray.rllib.core.models.torch.base import TorchModel
 from ray.rllib.models.torch.misc import SlimConv2d
-from ray.rllib.utils.typing import ModelConfigDict
 from torch import nn
+
+from .base import BaseModule
 
 module = sys.modules[__name__]
 
@@ -33,43 +29,13 @@ MOBILENET_INPUT_SHAPE = (3, 224, 224)
 #         pass
 
 
-# class MobileNetV2EncoderConfig(ModelConfig):
-#     # MobileNet v2 has a flat output with a length of 1000.
-#     output_dims = (1000,)
-#     freeze = True
-
-#     def build(self, framework):
-#         assert framework == "torch", "Unsupported framework `{}`!".format(framework)
-#         return MobileNetV2Encoder(self)
-
-
-# class MobileNetV2Encoder(TorchModel, Encoder):
-#     """A MobileNet v2 encoder for RLlib."""
-
-#     def __init__(self, config):
-#         super().__init__(config)
-#         self.net = torch.hub.load(
-#             "pytorch/vision:v0.6.0", "mobilenet_v2", pretrained=True
-#         )
-#         if config.freeze:
-#             # We don't want to train this encoder, so freeze its parameters!
-#             for p in self.net.parameters():
-#                 p.requires_grad = False
-
-#     def _forward(self, input_dict, **kwargs):
-#         return {ENCODER_OUT: (self.net(input_dict["obs"]))}
-
-
-class CNNEncoder(nn.Module):
+class CNNEncoder(BaseModule):
     """Generic CNN encoder."""
 
-    def __init__(
-        self,
-        model_config: ModelConfigDict,
-        input_shape: List[int],
-    ):
+    def __init__(self, model_config, obs_space):
         super().__init__()
 
+        input_shape = obs_space.shape
         activation = model_config.get("conv_activation")
         filters = model_config["conv_filters"]
         assert len(filters) > 0, "Must provide at least 1 entry in `conv_filters`!"
@@ -99,6 +65,9 @@ class CNNEncoder(nn.Module):
 
     def forward(self, inputs):
         # Obs is generally B x H x W x C, but CNNs expect B x C x H x W.
+        # Normalize
+        # FIXME: check gdino normalization
+        inputs = (inputs.to(torch.float32) / 128.0) - 1.0
         return self._convs(inputs.permute(0, 3, 1, 2))
 
     @property