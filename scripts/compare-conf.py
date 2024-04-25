06b5b07b0998fca91d13c11958cfada20b578b0c
Vince Jankovics <vincejankovics@gmail.com>
2024-04-25 09:56:54 +0100

    Compare conf change

--- scripts/compare-conf.py
+++ scripts/compare-conf.py
@@ -2,20 +2,24 @@ import argparse
 
 import yaml
 from ray.rllib.algorithms.ppo import PPOConfig
+from ray.tune.utils import flatten_dict
 
 
 def main(args):
     config1 = PPOConfig.from_dict(yaml.safe_load(open(args.config1))["base"]).to_dict()
     config2 = PPOConfig.from_dict(yaml.safe_load(open(args.config2))["base"]).to_dict()
 
+    config1 = flatten_dict(config1)
+    config2 = flatten_dict(config2)
+
     elements_1 = sorted(list(config1.keys()))
     elements_2 = sorted(list(config2.keys()))
 
-    assert set(elements_1) == set(elements_2)
+    keys = set(elements_1) | set(elements_2)
 
-    for element in elements_1:
-        value1 = config1[element]
-        value2 = config2[element]
+    for element in keys:
+        value1 = config1.get(element)
+        value2 = config2.get(element)
         if value1 != value2:
             print(f"!! {element}")
             print(f"Config 1: {value1}")
