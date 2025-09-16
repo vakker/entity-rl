a9b1b2438a5600a77e812c09dd66c5822932d8c2
Vince <vincejankovics@gmail.com>
2025-09-16 12:14:40 +0100

    Rename files

--- scripts/train_mot_vis.py
+++ scripts/train_mot_vis.py
@@ -16,7 +16,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm, trange
 
 from entity_rl import utils
-from entity_rl.datasets import MOTVisDataset
+from entity_rl.datasets import MOTVizDataset
 from entity_rl.models.enros import ENROSPolicy
 from entity_rl.training import (
     RewardLabelAdapter,
@@ -37,7 +37,7 @@ def main(args):
     print(f"Output directory: {args.output_dir}")
 
     # Create datasets
-    train_dataset = MOTVisDataset(
+    train_dataset = MOTVizDataset(
         mot_data_dirs=args.mot_dirs,
         agent_radius=args.agent_radius,
         num_samples_per_epoch=int(args.num_samples * 0.8),
@@ -45,7 +45,7 @@ def main(args):
         use_gt=not args.use_detections,
     )
 
-    val_dataset = MOTVisDataset(
+    val_dataset = MOTVizDataset(
         mot_data_dirs=args.mot_dirs,
         agent_radius=args.agent_radius,
         num_samples_per_epoch=int(args.num_samples * 0.2),
